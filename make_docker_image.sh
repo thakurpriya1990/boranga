6d2c1db07b234fe08cf50bb0267ed070818fe3b8
thakurpriya1990@gmail.com <thakurpriya1990@gmail.com>
2022-02-07 11:51:49 +0800

    revert back docker_image file

--- make_docker_image.sh
+++ make_docker_image.sh
@@ -1,84 +1,28 @@
 #!/bin/bash
-## first parameter is DBCA branch name, optional second parameter is an integer indicating incremental daily version
-set -e
-if [[ $# -lt 1 ]]; then
-    echo "ERROR: DBCA branch must be specified"
-    echo "$0 1"
-    exit 1
-fi
-if [[ $# -gt 1 ]] && ! [[ $2 =~ ^[0-9]+$ ]]; then
-    #echo "ERROR: Must specify integer indicating incremental daily version e.g."
-    echo "ERROR: Incremental daily version must be an integer"
-    echo "$0 1"
+## sole parameter is an integer indicating incremental daily version
+## git branch --set-upstream-to=origin/dev dev  
+
+if [ $# -lt 2 ]; then
+    echo "ERROR: Must specify <github branch> and <integer indicating incremental daily version> e.g."
+    echo "USAGE: $0 dev 1 <optional: --no-cache>"
     exit 1
 fi
 
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-REPO=$(basename -s .git `git config --get remote.origin.url` | sed 's/-//g')
-DBCA_BRANCH="dbca_"$1
-BUILD_TAG_NO_INCREMENT=dbcawa/$REPO:$1_v$(date +%Y.%m.%d)
-if [[ $# -gt 1 ]]; then
-    INCREMENT=$2
-else
-    INCREMENT=1
-    if [[ $(docker images | awk '{print $1":"$2}' | grep $BUILD_TAG_NO_INCREMENT) ]]; then
-        DAILY_IMAGE_INCREMENTS=$(docker images | awk '{print $1":"$2}' | grep $BUILD_TAG_NO_INCREMENT)
-        declare -i I=0
-        declare -A inc_array
-        for DAILY in $DAILY_IMAGE_INCREMENTS;
-        do
-            #INC=$(echo $DAILY | cut -c $((${#REPO}+21))-)
-            INC=$(echo $DAILY | cut -c $((${#REPO}+${#1}+22))-)
-            inc_array[$I]=$INC
-            I=$(($I+1))
-        done
-        declare -i max_value=0
-        for ii in ${inc_array[@]};
-        do
-            if [ $ii -gt $max_value ]; then
-                max_value=$ii
-            fi;
-        done
-        INCREMENT=$((max_value+1))
-    fi
+if [ $# -eq 3 ]; then
+    NO_CACHE=$3
 fi
-#BUILD_TAG=dbcawa/$REPO:v$(date +%Y.%m.%d).$INCREMENT
-BUILD_TAG=$BUILD_TAG_NO_INCREMENT.$INCREMENT
-{
-    git checkout $DBCA_BRANCH
-} ||
-{
-    echo "ERROR: You must have your local code checked in and the DBCA branch set up on local with the 'dbca_' prefix.  Example Instructions:"
-    echo "git remote add dbca git@github.com:dbca-wa/wildlifecompliance.git"
-    echo "git checkout -b dbca_compliance_mgt_dev dbca/compliance_mgt_dev"
-    echo "$0 1"
-    exit 1
-}
-{
-    git pull &&
-    cd $REPO/frontend/$REPO/ &&
-    npm run build &&
-    cd ../../../ &&
-    source venv/bin/activate &&
-    #./manage.py collectstatic --no-input &&
-    $(find . -maxdepth 1 -name "manage*.py") collectstatic --no-input &&
-    git log --pretty=medium -30 > ./git_history_recent &&
-    docker image build --no-cache --tag $BUILD_TAG . &&
-    git checkout $CURRENT_BRANCH
-    echo $BUILD_TAG
-} ||
-{
-    git checkout $CURRENT_BRANCH
-    echo "ERROR: Docker build failed"
-    echo "NB: This script assumes that your virtual environment folder is 'venv'"
-    echo "$0 1"
-    exit 1
-}
-{
-    docker push $BUILD_TAG
-} || {
-    git checkout $CURRENT_BRANCH
-    echo "ERROR: Docker push failed"
-    echo "$0 1"
-    exit 1
-}
+
+APP_ROOT=boranga
+GIT_BRANCH=$1
+#BUILD_TAG=dbcawa/$APP_ROOT:v$(date +%Y.%m.%d).$2
+BUILD_TAG=thakurpriya1990/$APP_ROOT:v$(date +%Y.%m.%d).$2
+#git checkout $GIT_BRANCH &&
+#git pull &&
+#cd $APP_ROOT/frontend/$APP_ROOT/
+#npm run build &&
+#cd ../../../ &&
+#source venv/bin/activate &&
+#./manage.py collectstatic --no-input &&
+docker image build $NO_CACHE --tag $BUILD_TAG . &&
+echo $BUILD_TAG &&
+docker push $BUILD_TAG
