01c0006550c7066a33dcca3984607bba3dc35fc5
Oak McIlwain <oak.mcilwain+dbca@gmail.com>
2023-02-20 16:19:15 +0800

    Test of dynamic bootstrap 5 tabs with child component rendering.

---
+++ boranga/frontend/boranga/src/components/internal/species_communities/species_split_tabs_child_component.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+
+        {{ species_name }} in a child component.
+
+    </div>
+</template>
+  
+  <script>
+
+    export default {
+        props:{
+            species_name: {
+                type: String,
+                required: true
+            },
+        },
+        data() { 
+            return {
+
+            }
+        },
+        components:{
+        },
+        methods: {
+
+        }
+    }; 
+  </script> 
+  
+  <style>
+
+  </style>
+  
