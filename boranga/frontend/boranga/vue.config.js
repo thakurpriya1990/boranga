9f80de2fb4a305282006ae209b7fc66c17b0a0c9
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2023-08-24 16:28:28 +0800

    Remove references to ckeditor 5 as it only works with vue3.

--- boranga/frontend/boranga/vue.config.js
+++ boranga/frontend/boranga/vue.config.js
@@ -3,10 +3,6 @@ const { defineConfig } = require('@vue/cli-service')
 const path = require('path');
 const webpack = require('webpack');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
-const {
-    CKEditorTranslationsPlugin,
-} = require('@ckeditor/ckeditor5-dev-translations');
-const { styles } = require('@ckeditor/ckeditor5-dev-utils');
 const port = process.env.PORT ? parseInt(process.env.PORT) : 9002;
 
 module.exports = defineConfig({
@@ -26,33 +22,7 @@ module.exports = defineConfig({
             '@static-root',
             path.resolve(__dirname, '../../../staticfiles_ll/')
         );
-
-        const svgRule = config.module.rule('svg');
-        svgRule.exclude.add(path.join(__dirname, 'node_modules', '@ckeditor'));
-        config.module
-            .rule('cke-svg')
-            .test(/ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/)
-            .use('raw-loader')
-            .loader('raw-loader');
-        config.module
-            .rule('cke-css')
-            .test(/ckeditor5-[^/\\]+[/\\].+\.css$/)
-            .use('postcss-loader')
-            .loader('postcss-loader')
-            .tap(() => {
-                return {
-                    postcssOptions: styles.getPostCssConfig({
-                        themeImporter: {
-                            themePath: require.resolve(
-                                '@ckeditor/ckeditor5-theme-lark'
-                            ),
-                        },
-                        minify: true,
-                    }),
-                };
-            });
     },
-    transpileDependencies: [/ckeditor5-[^/\\]+[/\\]src[/\\].+\.js$/],
     configureWebpack: {
         devtool: 'source-map',
         plugins: [
@@ -63,13 +33,6 @@ module.exports = defineConfig({
                 _: 'lodash',
             }),
             new MomentLocalesPlugin(),
-            new CKEditorTranslationsPlugin({
-                // See https://ckeditor.com/docs/ckeditor5/latest/features/ui-language.html
-                language: 'en',
-
-                // Append translations to the file matching the `app` name.
-                translationsOutputFile: /app/,
-            }),
         ],
         devServer: {
             host: '0.0.0.0',
