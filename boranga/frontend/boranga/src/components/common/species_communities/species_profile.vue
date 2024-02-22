037ad95928674f8785683d88d7712ffd15b7e4ce
Priya Thakur <thakurpriya1990@gmail.com>
2024-02-22 11:40:07 +0800

    resolve task 4700(resizable Name Authoruty field)

--- boranga/frontend/boranga/src/components/common/species_communities/species_profile.vue
+++ boranga/frontend/boranga/src/components/common/species_communities/species_profile.vue
@@ -82,7 +82,7 @@
             <div class="row mb-3">
                 <label for="" class="col-sm-3 control-label">Name Authority:</label>
                 <div class="col-sm-9">
-                    <input :disabled="true" type="text" class="form-control" id="name_authority" placeholder="" 
+                    <textarea :disabled="true" rows="1" class="form-control" id="name_authority" placeholder="" 
                     v-model="name_authority"/>
                 </div>
             </div>
