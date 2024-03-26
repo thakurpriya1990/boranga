10d611775ac9cb09ed51028db7442d453265308e
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-03-26 15:47:45 +0800

    Add new logic to check if the web server should be started.

--- pre_startup.sh
+++ pre_startup.sh
@@ -6,9 +6,18 @@ cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1 > /app/git_hash
 
 # elevated permissions required
 sudo /startup.sh
+
+if [ $ENABLE_WEB == "True" ];
+    then
+echo "Starting Gunicorn"
+# Start the second process
 gunicorn boranga.wsgi --bind :8080 --config /app/gunicorn.ini
 status=$?
 if [ $status -ne 0 ]; then
-    echo "Failed to start gunicorn: $status"
-    exit $status
+  echo "Failed to start gunicorn: $status"
+  exit $status
+fi
+else
+   echo "ENABLE_WEB environment vairable not set to True, web server is not starting."
+   /bin/bash
 fi
