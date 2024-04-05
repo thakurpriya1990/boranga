7d24cd05a6279795ba8eebc350379501fb36e0a7
Ashley <ash.woodiss-field@dbca.wa.gov.au>
2024-04-05 02:15:29 +0000

    Added: model-based authorisation for history (may be subject to review)

--- boranga/components/history/api.py
+++ boranga/components/history/api.py
@@ -4,7 +4,6 @@ from rest_framework.exceptions import PermissionDenied
 from rest_framework.mixins import ListModelMixin
 from rest_framework import views
 from reversion.models import Version
-from boranga.helpers import is_internal
 from rest_framework_datatables.pagination import PageNumberPagination, DatatablesPageNumberPagination
 import json
 from django.db.models import JSONField
@@ -12,17 +11,61 @@ from django.db.models.functions import Cast
 from rest_framework_datatables.filters import DatatablesFilterBackend
 from django.db.models import F
 from rest_framework_datatables.utils import get_param
+from boranga.helpers import (is_internal, is_customer,
+is_boranga_admin, is_django_admin, is_assessor, is_approver,
+is_species_processor, is_community_processor,
+is_conservation_status_editor)
 
 #keeping it as an APIView to control how its handled
 class InternalAuthorizationView(views.APIView):
     """ This ViewSet adds authorization that only allows internal users to
         return data.
     """
-    def get(self, request):
+    django_admin_models = ["Species","SpeciesDocument",
+                       "Community","CommunityDocument",
+                       "ConservationStatus","ConservationStatusDocument",
+                       "Occurrence", "OccurrenceReport"]
+    assessor_models = ["Species","SpeciesDocument",
+                       "Community","CommunityDocument",
+                       "ConservationStatus","ConservationStatusDocument",
+                       "Occurrence", "OccurrenceReport"]
+    approver_models = ["Species","SpeciesDocument",
+                       "Community","CommunityDocument",
+                       "ConservationStatus","ConservationStatusDocument",
+                       "Occurrence", "OccurrenceReport"]
+    species_processor_models = ["Species","SpeciesDocument"]
+    community_processor_models = [ "Community","CommunityDocument"]
+    conservation_status_editor_models = ["ConservationStatus","ConservationStatusDocument"]
+
+    #TODO other models to consider - Meetings, Minutes, and any Occurence Document Models
+
+    def check_auth_by_model(self, request, model_name):
+        if (request.user.is_superuser or
+            is_boranga_admin(request)):
+            return True
+        else:
+            #go through each list, if model is in it run function for user
+            #return the result if true, otherwise run other checks until all possibilities exhausted
+            if model_name in self.django_admin_models and is_assessor(request):
+                return True
+            if model_name in self.assessor_models and is_assessor(request):
+                return True
+            if model_name in self.approver_models and is_approver(request):
+                return True
+            if model_name in self.species_processor_models and is_species_processor(request):
+                return True
+            if model_name in self.community_processor_models and is_community_processor(request):
+                return True
+            if model_name in self.conservation_status_editor_models and is_conservation_status_editor(request):
+                return True
+
+            return False
+
+    def get(self, request, model_name):
         """ Deny access to the version history for external users """
-        #TODO utilise model specific permissions
-        if not is_internal(self.request):
+        if not is_internal(self.request) or not self.check_auth_by_model(request, model_name):
             raise PermissionDenied()
+
         
 class VersionsFilterBackend(DatatablesFilterBackend):
 
@@ -119,7 +162,7 @@ class GetPaginatedVersionsView(InternalAuthorizationView):
 
             api/history/boranga/species_communities/SpeciesDocument/729
         """
-        super().get(self)
+        super().get(request,model_name)
 
         model = apps.get_model(app_label=app_label, model_name=model_name)
         instance = model.objects.get(pk=int(pk))
@@ -154,21 +197,21 @@ class GetPaginatedVersionsView(InternalAuthorizationView):
     
 class GetRevisionVersionsView(InternalAuthorizationView):
 
-    def get(self, request, primary_model_name, revision_id):
+    def get(self, request, model_name, revision_id):
         """ Returns all versions for a revision id, oriented by a primary model
 
-            api/history/primary_model_name/revision_id/
+            api/history/model_name/revision_id/
 
             Example:
 
             api/history/SpeciesDocument/112/
         """
-        super().get(self)
+        super().get(request,model_name)
 
         queryset = Version.objects.filter(revision_id=revision_id).annotate(data=Cast('serialized_data', JSONField()))
 
         # Build the list of versions
-        primary_version = queryset.get(content_type__model__iexact=primary_model_name)
+        primary_version = queryset.get(content_type__model__iexact=model_name)
         revision_dict = {
             'revision_id': primary_version.revision_id,
             'date_created': primary_version.revision.date_created.strftime("%Y-%m-%d %H:%M:%S"),
@@ -179,7 +222,7 @@ class GetRevisionVersionsView(InternalAuthorizationView):
             version_data[primary_version.content_type.model.lower()]["model_display_name"] = primary_version.content_type.name
 
         for version in queryset:
-            if not version.content_type.model.lower() == primary_model_name.lower() and version.data:
+            if not version.content_type.model.lower() == model_name.lower() and version.data:
                 version_data[version.content_type.model.lower()] = version.data[0]
                 version_data[version.content_type.model.lower()]["model_display_name"] = version.content_type.name
 
