f68fe89d627f0942bb38404ae3cb059aaf5eab09
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-04-23 17:32:46 +0800

    Add email_in_dept_domains function.

--- boranga/helpers.py
+++ boranga/helpers.py
@@ -1,14 +1,21 @@
-from __future__ import unicode_literals
-from ledger_api_client.ledger_models import EmailUserRO as EmailUser, EmailUserRO
-from ledger_api_client.managed_models import SystemGroup
+import logging
+
+import ledger_api_client
 from django.conf import settings
 from django.core.cache import cache
+from ledger_api_client.ledger_models import EmailUserRO
+from ledger_api_client.ledger_models import EmailUserRO as EmailUser
+from ledger_api_client.managed_models import SystemGroup
 
-from boranga.settings import GROUP_NAME_ASSESSOR, GROUP_NAME_APPROVER, GROUP_NAME_SPECIES_COMMUNITIES_PROCESSOR,GROUP_NAME_EDITOR
+from boranga.settings import (
+    GROUP_NAME_APPROVER,
+    GROUP_NAME_ASSESSOR,
+    GROUP_NAME_EDITOR,
+    GROUP_NAME_SPECIES_COMMUNITIES_PROCESSOR,
+)
 
-import logging
-import ledger_api_client
 logger = logging.getLogger(__name__)
+
 
 def belongs_to(user, group_name):
     """
@@ -17,23 +24,30 @@ def belongs_to(user, group_name):
     :param group_name:
     :return:
     """
-    #import ipdb; ipdb.set_trace()
-    belongs_to_value = cache.get('User-belongs_to'+str(user.id)+'group_name:'+group_name)
+    # import ipdb; ipdb.set_trace()
+    belongs_to_value = cache.get(
+        "User-belongs_to" + str(user.id) + "group_name:" + group_name
+    )
     if belongs_to_value:
-        print ('From Cache - User-belongs_to'+str(user.id)+'group_name:'+group_name)
+        print(
+            "From Cache - User-belongs_to" + str(user.id) + "group_name:" + group_name
+        )
     if belongs_to_value is None:
-            belongs_to_value = False
-            system_group = ledger_api_client.managed_models.SystemGroup.objects.get(name=group_name)
-            if user.id in system_group.get_system_group_member_ids():
-                belongs_to_value = True
-            cache.set(
-                "User-belongs_to" + str(user.id) + "group_name:" + group_name,
-                belongs_to_value,
-                3600,
-            )
+        belongs_to_value = False
+        system_group = ledger_api_client.managed_models.SystemGroup.objects.get(
+            name=group_name
+        )
+        if user.id in system_group.get_system_group_member_ids():
+            belongs_to_value = True
+        cache.set(
+            "User-belongs_to" + str(user.id) + "group_name:" + group_name,
+            belongs_to_value,
+            3600,
+        )
     return belongs_to_value
 
-    #return user.groups.filter(name=group_name).exists()
+    # return user.groups.filter(name=group_name).exists()
+
 
 # def is_model_backend(request):
 #    # Return True if user logged in via single sign-on (i.e. an internal)
@@ -43,13 +57,21 @@ def belongs_to(user, group_name):
 #    # Return True if user logged in via social_auth (i.e. an external user signing in with a login-token)
 #    return 'EmailAuth' in request.session.get('_auth_user_backend')
 
+
 def is_boranga_admin(request):
-    #import ipdb; ipdb.set_trace()
-    #logger.info('settings.ADMIN_GROUP: {}'.format(settings.ADMIN_GROUP))
-    return request.user.is_authenticated and (belongs_to(request.user, settings.ADMIN_GROUP) or request.user.is_superuser)
+    # import ipdb; ipdb.set_trace()
+    # logger.info('settings.ADMIN_GROUP: {}'.format(settings.ADMIN_GROUP))
+    return request.user.is_authenticated and (
+        belongs_to(request.user, settings.ADMIN_GROUP) or request.user.is_superuser
+    )
+
 
 def is_django_admin(request):
-    return request.user.is_authenticated and (belongs_to(request.user, settings.DJANGO_ADMIN_GROUP) or request.user.is_superuser)
+    return request.user.is_authenticated and (
+        belongs_to(request.user, settings.DJANGO_ADMIN_GROUP)
+        or request.user.is_superuser
+    )
+
 
 def is_assessor(user_id):
     if isinstance(user_id, EmailUser) or isinstance(user_id, EmailUserRO):
@@ -64,17 +86,24 @@ def is_approver(user_id):
     assessor_group = SystemGroup.objects.get(name=GROUP_NAME_APPROVER)
     return True if user_id in assessor_group.get_system_group_member_ids() else False
 
+
 def is_species_processor(user_id):
     if isinstance(user_id, EmailUser) or isinstance(user_id, EmailUserRO):
         user_id = user_id.id
-    species_group = SystemGroup.objects.get(name=GROUP_NAME_SPECIES_COMMUNITIES_PROCESSOR)
+    species_group = SystemGroup.objects.get(
+        name=GROUP_NAME_SPECIES_COMMUNITIES_PROCESSOR
+    )
     return True if user_id in species_group.get_system_group_member_ids() else False
+
 
 def is_community_processor(user_id):
     if isinstance(user_id, EmailUser) or isinstance(user_id, EmailUserRO):
         user_id = user_id.id
-    community_group = SystemGroup.objects.get(name=GROUP_NAME_SPECIES_COMMUNITIES_PROCESSOR)
+    community_group = SystemGroup.objects.get(
+        name=GROUP_NAME_SPECIES_COMMUNITIES_PROCESSOR
+    )
     return True if user_id in community_group.get_system_group_member_ids() else False
+
 
 def is_conservation_status_referee(request, cs_proposal=None):
     from boranga.components.conservation_status.models import ConservationStatusReferral
@@ -85,35 +114,49 @@ def is_conservation_status_referee(request, cs_proposal=None):
 
     return qs.exists()
 
+
 def is_conservation_status_editor(user_id):
     if isinstance(user_id, EmailUser) or isinstance(user_id, EmailUserRO):
         user_id = user_id.id
     assessor_group = SystemGroup.objects.get(name=GROUP_NAME_EDITOR)
     return True if user_id in assessor_group.get_system_group_member_ids() else False
 
+
 def in_dbca_domain(request):
     user = request.user
-    domain = user.email.split('@')[1]
+    domain = user.email.split("@")[1]
     if domain in settings.DEPT_DOMAINS:
         if not user.is_staff:
-            # hack to reset department user to is_staff==True, if the user logged in externally (external departmentUser login defaults to is_staff=False)
+            # hack to reset department user to is_staff==True, if the user logged in externally
+            # (external departmentUser login defaults to is_staff=False)
             user.is_staff = True
             user.save()
         return True
     return False
 
+
+def email_in_dept_domains(email):
+    return email.split("@")[1] in settings.DEPT_DOMAINS
+
+
 def is_in_organisation_contacts(request, organisation):
-    return request.user.email in organisation.contacts.all().values_list('email', flat=True)
+    return request.user.email in organisation.contacts.all().values_list(
+        "email", flat=True
+    )
+
 
 def is_departmentUser(request):
-    #return request.user.is_authenticated and is_model_backend(request) and in_dbca_domain(request)
+    # return request.user.is_authenticated and is_model_backend(request) and in_dbca_domain(request)
     return request.user.is_authenticated and in_dbca_domain(request)
+
 
 def is_customer(request):
     return request.user.is_authenticated and not request.user.is_staff
 
+
 def is_internal(request):
     return is_departmentUser(request)
+
 
 def get_all_officers():
     return EmailUser.objects.filter(groups__name=settings.ADMIN_GROUP)
