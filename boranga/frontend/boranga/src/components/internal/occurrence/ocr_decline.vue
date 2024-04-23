8726dbf65817c341102b3122a8e6a20f2364c3c0
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-04-23 16:00:45 +0800

    Add decline modal.

---
+++ boranga/frontend/boranga/src/components/internal/occurrence/ocr_decline.vue
@@ -0,0 +1,123 @@
+<template lang="html">
+    <div id="internal-ocr-decline-request">
+        <modal id="myModal" transition="modal fade" @ok="ok()" ok-text="Decline" @cancel="close()"
+            :title="`Decline ${occurrence_report_number}`" large>
+            <div class="container-fluid">
+                <div class="row">
+                    <form class="form-horizontal" name="decline-form">
+                        <alert v-if="errorString" type="danger"><strong>{{ errorString }}</strong></alert>
+                        <label class="control-label mb-3" for="reason">Reason</label>
+                        <textarea class="form-control" name="reason" v-model="decline.reason" id="reason"
+                            ref="reason" required></textarea>
+                    </form>
+                </div>
+            </div>
+        </modal>
+    </div>
+</template>
+
+<script>
+import modal from '@vue-utils/bootstrap-modal.vue'
+import alert from '@vue-utils/alert.vue'
+
+import { helpers, api_endpoints } from "@/utils/hooks.js"
+export default {
+    name: 'ocr-decline',
+    components: {
+        modal,
+        alert,
+    },
+    props: {
+        occurrence_report_id: {
+            type: Number,
+        },
+        occurrence_report_number: {
+            type: String,
+        },
+        declined_details: {
+            type: Object,
+        },
+    },
+    data: function () {
+        let vm = this;
+        return {
+            isModalOpen: false,
+            form: null,
+            decline: {
+                reason: '',
+            },
+            errorString: '',
+        }
+    },
+    watch: {
+        isModalOpen: function (val) {
+            if (val) {
+                this.$nextTick(() => {
+                    this.$refs.reason.focus();
+                    if(this.declined_details){
+                        this.decline.reason = this.declined_details.reason;
+                    }
+                });
+            }
+        },
+    },
+    methods: {
+        ok: function () {
+            let vm = this;
+            if (vm.form.checkValidity()) {
+                vm.proposeDecline();
+            } else {
+                vm.form.reportValidity();
+            }
+        },
+        close: function () {
+            this.isModalOpen = false;
+            this.decline = {
+                reason: '',
+                occurrence_report: this.occurrence_report_id
+            };
+            this.errorString = '';
+            this.form.reset();
+        },
+        proposeDecline: function () {
+            let vm = this;
+            swal.fire({
+                title: "Decline Occurrence Report",
+                text: `Are you sure you want to decline occurrence report ${vm.occurrence_report_number}?`,
+                icon: "question",
+                showCancelButton: true,
+                confirmButtonText: 'Decline',
+                confirmButtonColor: '#d9534f',
+                reverseButtons: true,
+                buttonsStyling: false,
+                customClass: {
+                    confirmButton: 'btn btn-primary',
+                    cancelButton: 'btn btn-secondary me-2',
+                },
+            }).then((swalresult) => {
+                if (swalresult.isConfirmed) {
+                    vm.$http.post(helpers.add_endpoint_join(api_endpoints.occurrence_report, '/' + (vm.occurrence_report_id + '/decline/')), JSON.stringify(vm.decline))
+                        .then((response) => {
+                            swal.fire({
+                                title: 'Occurrence Report Successfully Declined',
+                                text: `Occurrence Report ${vm.occurrence_report_number} has been succuessfully declined. The applicant has been notified via email.`,
+                                icon: 'success',
+                                confirmButtonColor: '#226fbb',
+                            });
+                            vm.$router.push({
+                                name: 'internal-occurrence-dash'
+                            });
+                        }, (error) => {
+                            console.log(error);
+                            vm.errorString = helpers.apiVueResourceError(error);
+                            vm.amendingProposal = true;
+                        });
+                }
+            });
+        },
+    },
+    mounted: function () {
+        this.form = document.forms['decline-form'];
+    }
+}
+</script>
