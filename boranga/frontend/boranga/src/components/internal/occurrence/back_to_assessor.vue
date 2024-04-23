43da527ae8d6b5f30ad9c49030601ab5f6514b04
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-04-23 12:12:38 +0800

    Add ocr back to assessor modal.

---
+++ boranga/frontend/boranga/src/components/internal/occurrence/back_to_assessor.vue
@@ -0,0 +1,118 @@
+<template lang="html">
+    <div id="internal-ocr-back-to-assessor">
+        <modal id="myModal" transition="modal fade" @ok="ok()" @cancel="cancel()" title="Back to Assessor" okText="Back to Assessor" large>
+            <div class="container-fluid">
+                <div class="row">
+                    <form class="form-horizontal" name="back-to-assessor-form">
+                        <alert v-if="errorString" type="danger"><strong>{{ errorString }}</strong></alert>
+                        <div class="col-sm-12">
+                            <div class="row mb-3">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label class="control-label mb-3" for="reason">Reason</label>
+                                        <textarea class="form-control" name="reason" v-model="back_to_assessor.text"
+                                            id="reason" ref="reason" required></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </modal>
+    </div>
+</template>
+
+<script>
+
+import Vue from 'vue'
+import modal from '@vue-utils/bootstrap-modal.vue'
+import alert from '@vue-utils/alert.vue'
+
+import { helpers, api_endpoints } from "@/utils/hooks.js"
+import { occurrence } from '../../../api'
+export default {
+    name: 'back-to-assessor',
+    components: {
+        modal,
+        alert,
+    },
+    props: {
+        occurrence_report_id: {
+            type: Number,
+        },
+        occurrence_report_number: {
+            type: String,
+        },
+    },
+    data: function () {
+        let vm = this;
+        return {
+            isModalOpen: false,
+            form: null,
+            back_to_assessor: {
+                reason: '',
+            },
+            errorString: '',
+        }
+    },
+    watch: {
+        isModalOpen: function (val) {
+            if (val) {
+                this.$nextTick(() => {
+                    this.$refs.reason.focus();
+                });
+            }
+        }
+    },
+    methods: {
+        ok: function () {
+            if (this.form.checkValidity()) {
+                this.backToAssessor();
+            } else {
+                this.form.reportValidity();
+            }
+        },
+        cancel: function () {
+            this.close();
+        },
+        close: function () {
+            this.isModalOpen = false;
+            this.back_to_assessor = {
+                reason: '',
+            };
+            this.errorString = '';
+        },
+        backToAssessor: function () {
+            let vm = this;
+            vm.errorString = '';
+            vm.$http.post(api_endpoints.occurrence_report + `/${vm.occurrence_report_id}/back_to_assessor/`, JSON.stringify(vm.back_to_assessor), {
+                emulateJSON: true,
+            }).then((response) => {
+                swal.fire({
+                    title: 'Sent Back',
+                    text: `${bm.occurrence_report_number} has been sent back to the assessor with the provided reason.`,
+                    icon: 'success',
+                    confirmButtonColor: '#226fbb'
+                });
+                vm.close();
+
+                Vue.http.get(`/api/occurrence_report/${vm.occurrence_report_id}/internal_occurrence_report.json`).then((response) => {
+                    vm.$emit('refreshFromResponse', response);
+
+                }, (error) => {
+                    console.log(error);
+                });
+                vm.$router.push({ path: '/internal/occurrence' });
+
+            }, (error) => {
+                console.log(error);
+                vm.errorString = helpers.apiVueResourceError(error);
+            });
+        },
+    },
+    mounted: function () {
+        this.form = document.forms['back-to-assessor-form'];
+    }
+}
+</script>
