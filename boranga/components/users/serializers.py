5b095137aa1d84705cedce7f5376519ca384bc08
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-04-29 09:54:24 +0800

    Uncomment required serializers.

--- boranga/components/users/serializers.py
+++ boranga/components/users/serializers.py
@@ -1,50 +1,35 @@
 from django.conf import settings
-from ledger_api_client.ledger_models import (
-        EmailUserRO as EmailUser,Address, #Profile,
-        EmailIdentity, 
-        #EmailUserAction, EmailUserLogEntry
-        )
-# from boranga.components.organisations.models import Organisation
-from boranga.components.main.models import ( 
-    UserSystemSettings, 
-    Document, 
-    # ApplicationType, 
-    CommunicationsLogEntry
-)
-# from boranga.components.proposals.models import Proposal
-from boranga.components.organisations.utils import can_admin_org, is_consultant
-from boranga.helpers import is_boranga_admin, in_dbca_domain
+from ledger_api_client.ledger_models import Address
+from ledger_api_client.ledger_models import EmailUserRO as EmailUser
 from rest_framework import serializers
-#from ledger.payments.helpers import is_payment_admin
-from django.utils import timezone
-from datetime import date, timedelta
-# from boranga.components.approvals.models import Approval
+
+from boranga.components.main.models import (
+    CommunicationsLogEntry,
+    Document,
+    UserSystemSettings,
+)
+from boranga.components.users.models import EmailUserAction, EmailUserLogEntry
+from boranga.helpers import in_dbca_domain, is_boranga_admin
 
 
 class DocumentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Document
-        fields = ('id','description','file','name','uploaded_date')
+        fields = ("id", "description", "file", "name", "uploaded_date")
+
 
 class UserAddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
-        fields = (
-            'id',
-            'line1',
-            'locality',
-            'state',
-            'country',
-            'postcode'
-        )
+        fields = ("id", "line1", "locality", "state", "country", "postcode")
+
 
 class UserSystemSettingsSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserSystemSettings
-        fields = (
-            'one_row_per_park',
-        )
+        fields = ("one_row_per_park",)
+
 
 # class UserOrganisationSerializer(serializers.ModelSerializer):
 #     name = serializers.CharField(source='organisation.name')
@@ -84,7 +69,8 @@ class UserSystemSettingsSerializer(serializers.ModelSerializer):
 #         _list = []
 #         #for application_type in ['T Class', 'Filming', 'Event']:
 #         for application_type in [ApplicationType.TCLASS, ApplicationType.FILMING, ApplicationType.EVENT ]:
-#             qs = Proposal.objects.filter(application_type__name=application_type, org_applicant=obj).exclude(processing_status__in=['approved', 'declined', 'discarded']).values_list('lodgement_number', flat=True)
+#             qs = Proposal.objects.filter(application_type__name=application_type, org_applicant=obj)
+# .exclude(processing_status__in=['approved', 'declined', 'discarded']).values_list('lodgement_number', flat=True)
 #             _list.append( dict(application_type=application_type, proposals=qs) )
 #         return _list
 
@@ -94,11 +80,11 @@ class UserSystemSettingsSerializer(serializers.ModelSerializer):
 #         year_date = today - timedelta(days=365)
 #         _list = []
 #         #for application_type in ['T Class', 'Filming', 'Event']:
-#         qs = Approval.objects.filter(expiry_date__lte=today, expiry_date__gte=year_date,current_proposal__application_type__name=ApplicationType.EVENT, current_proposal__org_applicant=obj).values('id','current_proposal','current_proposal__event_activity__event_name').order_by('id')
+#         qs = Approval.objects.filter(expiry_date__lte=today, expiry_date__gte=year_date,
+# current_proposal__application_type__name=ApplicationType.EVENT, current_proposal__org_applicant=obj)
+# .values('id','current_proposal','current_proposal__event_activity__event_name').order_by('id')
 #         _list.append( dict(application_type=ApplicationType.EVENT, proposals=qs) )
 #         return _list
-
-
 
 
 class UserFilterSerializer(serializers.ModelSerializer):
@@ -106,61 +92,49 @@ class UserFilterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = EmailUser
-        fields = (
-            'id',
-            'last_name',
-            'first_name',
-            'email',
-            'name'
-        )
+        fields = ("id", "last_name", "first_name", "email", "name")
 
     def get_name(self, obj):
         return obj.get_full_name()
 
 
 class UserSerializer(serializers.ModelSerializer):
-    # boranga_organisations = serializers.SerializerMethodField()
     residential_address = UserAddressSerializer()
     personal_details = serializers.SerializerMethodField()
     address_details = serializers.SerializerMethodField()
     contact_details = serializers.SerializerMethodField()
     full_name = serializers.SerializerMethodField()
     is_department_user = serializers.SerializerMethodField()
-    #is_payment_admin = serializers.SerializerMethodField()
-    system_settings= serializers.SerializerMethodField()
-    # is_payment_admin = serializers.SerializerMethodField()
-    is_boranga_admin = serializers.SerializerMethodField()    
+    system_settings = serializers.SerializerMethodField()
+    is_boranga_admin = serializers.SerializerMethodField()
 
     class Meta:
         model = EmailUser
         fields = (
-            'id',
-            'last_name',
-            'first_name',
-            'email',
-            #'identification',
-            'residential_address',
-            'phone_number',
-            'mobile_number',
-            # 'boranga_organisations',
-            'personal_details',
-            'address_details',
-            'contact_details',
-            'full_name',
-            'is_department_user',
-            #'is_payment_admin',
-            'is_staff',
-            'system_settings',
-            'is_boranga_admin',
+            "id",
+            "last_name",
+            "first_name",
+            "email",
+            "residential_address",
+            "phone_number",
+            "mobile_number",
+            "personal_details",
+            "address_details",
+            "contact_details",
+            "full_name",
+            "is_department_user",
+            "is_staff",
+            "system_settings",
+            "is_boranga_admin",
         )
 
-    def get_personal_details(self,obj):
-        return True if obj.last_name  and obj.first_name else False
+    def get_personal_details(self, obj):
+        return True if obj.last_name and obj.first_name else False
 
-    def get_address_details(self,obj):
+    def get_address_details(self, obj):
         return True if obj.residential_address else False
 
-    def get_contact_details(self,obj):
+    def get_contact_details(self, obj):
         if obj.mobile_number and obj.email:
             return True
         elif obj.phone_number and obj.email:
@@ -180,8 +154,8 @@ class UserSerializer(serializers.ModelSerializer):
         else:
             return False
 
-    #def get_is_payment_admin(self, obj):
-     #   return is_payment_admin(obj)
+    # def get_is_payment_admin(self, obj):
+    #   return is_payment_admin(obj)
 
     # def get_boranga_organisations(self, obj):
     #     boranga_organisations = obj.boranga_organisations
@@ -194,13 +168,14 @@ class UserSerializer(serializers.ModelSerializer):
         try:
             user_system_settings = obj.system_settings.first()
             serialized_settings = UserSystemSettingsSerializer(
-                user_system_settings).data
+                user_system_settings
+            ).data
             return serialized_settings
-        except:
+        except Exception:
             return None
 
     def get_is_boranga_admin(self, obj):
-        request = self.context['request'] if self.context else None
+        request = self.context["request"] if self.context else None
         if request:
             return is_boranga_admin(request)
         return False
@@ -210,77 +185,86 @@ class PersonalSerializer(serializers.ModelSerializer):
     class Meta:
         model = EmailUser
         fields = (
-            'id',
-            'last_name',
-            'first_name',
+            "id",
+            "last_name",
+            "first_name",
         )
+
 
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = EmailUser
         fields = (
-            'id',
-            'email',
-            'phone_number',
-            'mobile_number',
+            "id",
+            "email",
+            "phone_number",
+            "mobile_number",
         )
 
     def validate(self, obj):
-        #Mobile and phone number for dbca user are updated from active directory so need to skip these users from validation.
-        domain=None
-        if obj['email']:
-            domain = obj['email'].split('@')[1]
+        # Mobile and phone number for dbca user are updated from active directory
+        # so need to skip these users from validation.
+        domain = None
+        if obj["email"]:
+            domain = obj["email"].split("@")[1]
         if domain in settings.DEPT_DOMAINS:
             return obj
         else:
-            if not obj.get('phone_number') and not obj.get('mobile_number'):
-                raise serializers.ValidationError('You must provide a mobile/phone number')
+            if not obj.get("phone_number") and not obj.get("mobile_number"):
+                raise serializers.ValidationError(
+                    "You must provide a mobile/phone number"
+                )
         return obj
 
-#class EmailUserActionSerializer(serializers.ModelSerializer):
-#    who = serializers.CharField(source='who.get_full_name')
-#
-#    class Meta:
-#        model = EmailUserAction
-#        fields = '__all__'
 
-#class EmailUserCommsSerializer(serializers.ModelSerializer):
-#    class Meta:
-#        model = EmailUserLogEntry
-#        fields = '__all__'
+class EmailUserActionSerializer(serializers.ModelSerializer):
+    who = serializers.CharField(source="who.get_full_name")
+
+    class Meta:
+        model = EmailUserAction
+        fields = "__all__"
+
+
+class EmailUserCommsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmailUserLogEntry
+        fields = "__all__"
+
 
 class CommunicationLogEntrySerializer(serializers.ModelSerializer):
-    customer = serializers.PrimaryKeyRelatedField(queryset=EmailUser.objects.all(),required=False)
+    customer = serializers.PrimaryKeyRelatedField(
+        queryset=EmailUser.objects.all(), required=False
+    )
     documents = serializers.SerializerMethodField()
+
     class Meta:
         model = CommunicationsLogEntry
         fields = (
-            'id',
-            'customer',
-            'to',
-            'fromm',
-            'cc',
-            'log_type',
-            'reference',
-            'subject'
-            'text',
-            'created',
-            'staff',
-            'emailuser',
-            'documents'
+            "id",
+            "customer",
+            "to",
+            "fromm",
+            "cc",
+            "log_type",
+            "reference",
+            "subject" "text",
+            "created",
+            "staff",
+            "emailuser",
+            "documents",
         )
 
-    def get_documents(self,obj):
-        return [[d.name,d._file.url] for d in obj.documents.all()]
+    def get_documents(self, obj):
+        return [[d.name, d._file.url] for d in obj.documents.all()]
 
-#class EmailUserLogEntrySerializer(CommunicationLogEntrySerializer):
-#    documents = serializers.SerializerMethodField()
-#    class Meta:
-#        model = EmailUserLogEntry
-#        fields = '__all__'
-#        read_only_fields = (
-#            'customer',
-#        )
-#
-#    def get_documents(self,obj):
-#        return [[d.name,d._file.url] for d in obj.documents.all()]
+
+class EmailUserLogEntrySerializer(CommunicationLogEntrySerializer):
+    documents = serializers.SerializerMethodField()
+
+    class Meta:
+        model = EmailUserLogEntry
+        fields = "__all__"
+        read_only_fields = ("customer",)
+
+    def get_documents(self, obj):
+        return [[d.name, d._file.url] for d in obj.documents.all()]
