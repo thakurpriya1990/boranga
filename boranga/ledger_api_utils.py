7581c296598bdec4bb3c166273a2b54309975106
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-04-30 15:26:53 +0800

    Add basic error handling to retrieve_email_user function.

--- boranga/ledger_api_utils.py
+++ boranga/ledger_api_utils.py
@@ -1,12 +1,17 @@
-from __future__ import unicode_literals
-from ledger_api_client.ledger_models import EmailUserRO as EmailUser
-from boranga.components.main.decorators import basic_exception_handler
-
 import logging
+
+from ledger_api_client.ledger_models import EmailUserRO as EmailUser
+
+from boranga.components.main.decorators import basic_exception_handler
 
 logger = logging.getLogger("boranga")
 
 
 @basic_exception_handler
 def retrieve_email_user(email_user_id):
-    return EmailUser.objects.get(id=email_user_id)
+    try:
+        email_user = EmailUser.objects.get(id=email_user_id)
+    except EmailUser.DoesNotExist:
+        logger.error(f"EmailUser with id {email_user_id} does not exist")
+        return None
+    return email_user
