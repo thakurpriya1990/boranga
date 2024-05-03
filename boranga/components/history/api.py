069580025fd1e8da7b4f4722057331cc9660ce1a
Ashley <ash.woodiss-field@dbca.wa.gov.au>
2024-05-03 02:24:33 +0000

    Update: adjusted history api to account for OCR - OCC relations, we want to show the OCR OCC but not all the other related OCRs (for table only, details tbd)

--- boranga/components/history/api.py
+++ boranga/components/history/api.py
@@ -9,7 +9,7 @@ from rest_framework_datatables.pagination import DatatablesPageNumberPagination
 from django.db.models import JSONField
 from django.db.models.functions import Cast
 from rest_framework_datatables.filters import DatatablesFilterBackend
-from django.db.models import F
+from django.db.models import F, Q
 from rest_framework_datatables.utils import get_param
 from boranga.helpers import (is_internal, is_customer,
 is_boranga_admin, is_django_admin, is_assessor, is_approver,
@@ -262,7 +262,12 @@ class GetPaginatedVersionsView(InternalAuthorizationView):
 
         # Build the list of versions
         versions_list = []
-        related_versions = Version.objects.annotate(data=Cast('serialized_data', JSONField()))
+        primary_content_type = ""
+        if queryset.count() > 1:
+            primary_content_type = queryset[0].content_type
+
+        #we do not want to present multiple versions of the same object
+        related_versions = Version.objects.annotate(data=Cast('serialized_data', JSONField())).exclude(Q(content_type=primary_content_type)&~Q(object_id=pk))
 
         paginated_queryset = Version.objects.filter(id__in=list(map(lambda i:i.pk,queryset_list)))
         self.lookup_getter.getVersionModelLookUpFieldValues(paginated_queryset,model)
@@ -281,7 +286,7 @@ class GetPaginatedVersionsView(InternalAuthorizationView):
                     data[related_version.content_type.model] = related_version.data[0]
 
             #get lookup fields (for main model only)
-            if model._meta.model_name in data:       
+            if model._meta.model_name in data:
                 for i in self.lookup_getter.lookup_fields:
                     if i in data[model._meta.model_name]["fields"] and data[model._meta.model_name]["fields"][i] != None:
                         data[model._meta.model_name]["fields"][i] = self.lookup_getter.lookup_values[i][data[model._meta.model_name]["fields"][i]]
