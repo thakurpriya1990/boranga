11a423f1b11fb6e8065c575995421780e05cf79a
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-05-10 15:42:04 +0800

    Modify to work with new structure of GROUP_NAME_CHOICES.

--- boranga/management/default_data_manager.py
+++ boranga/management/default_data_manager.py
@@ -10,15 +10,15 @@ class DefaultDataManager:
     def __init__(self):
         for group_name in settings.GROUP_NAME_CHOICES:
             try:
-                group, created = SystemGroup.objects.get_or_create(name=group_name[0])
+                group, created = SystemGroup.objects.get_or_create(name=group_name)
                 if created:
                     logger.info(f"Created SystemGroup: {group}")
             except Exception as e:
-                logger.error(f"{e}, SystemGroup: {group_name[0]}")
+                logger.error(f"{e}, SystemGroup: {group_name}")
 
         # Delete any groups that are not in the settings.GROUP_NAME_CHOICES
         for sg in SystemGroup.objects.exclude(
-            name__in=[group_name[0] for group_name in settings.GROUP_NAME_CHOICES]
+            name__in=[group_name for group_name in settings.GROUP_NAME_CHOICES]
         ):
             sg.systemgrouppermission_set.all().delete()
             sg.delete()
