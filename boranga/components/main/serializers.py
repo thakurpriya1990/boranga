1ab878363e5269346947e0b87c378b71adff0d5d
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-05-13 14:19:34 +0800

    Remove references to unused model AmendmentReason and RequiredDocument.

--- boranga/components/main/serializers.py
+++ boranga/components/main/serializers.py
@@ -1,95 +1,65 @@
+from ledger_api_client.ledger_models import EmailUserRO
+from ledger_api_client.ledger_models import EmailUserRO as EmailUser
 from rest_framework import serializers
-from django.db.models import Sum, Max
-from boranga.components.main.models import (
-        CommunicationsLogEntry, 
-        # RequiredDocument, 
-        # Question, 
-        GlobalSettings, 
-        # ApplicationType,
-        )
-from ledger_api_client.ledger_models import EmailUserRO as EmailUser, EmailUserRO
-from datetime import datetime, date
-#from boranga.components.proposals.serializers import ProposalTypeSerializer
+
+from boranga.components.main.models import CommunicationsLogEntry, GlobalSettings
+
 
 class CommunicationLogEntrySerializer(serializers.ModelSerializer):
-    customer = serializers.PrimaryKeyRelatedField(queryset=EmailUser.objects.all(),required=False)
+    customer = serializers.PrimaryKeyRelatedField(
+        queryset=EmailUser.objects.all(), required=False
+    )
     documents = serializers.SerializerMethodField()
+
     class Meta:
         model = CommunicationsLogEntry
         fields = (
-            'id',
-            'customer',
-            'to',
-            'fromm',
-            'cc',
-            'type',
-            'reference',
-            'subject'
-            'text',
-            'created',
-            'staff',
-            'proposal'
-            'documents'
+            "id",
+            "customer",
+            "to",
+            "fromm",
+            "cc",
+            "type",
+            "reference",
+            "subject" "text",
+            "created",
+            "staff",
+            "proposal" "documents",
         )
 
-    def get_documents(self,obj):
-        return [[d.name,d._file.url] for d in obj.documents.all()]
-
-
-# class ApplicationTypeSerializer(serializers.ModelSerializer):
-#     #regions = RegionSerializer(many=True)
-#     #activity_app_types = ActivitySerializer(many=True)
-#     #tenure_app_types = TenureSerializer(many=True)
-#     class Meta:
-#         model = ApplicationType
-#         #fields = ('id', 'name', 'activity_app_types', 'tenure_app_types')
-#         #fields = ('id', 'name', 'tenure_app_types')
-#         fields = '__all__'
-#         #extra_fields = ['pizzas']
+    def get_documents(self, obj):
+        return [[d.name, d._file.url] for d in obj.documents.all()]
 
 
 class GlobalSettingsSerializer(serializers.ModelSerializer):
     class Meta:
         model = GlobalSettings
-        fields = ('key', 'value')
-
-
-
-# class RequiredDocumentSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = RequiredDocument
-#         fields = ('id', 'park','activity', 'question')
-
-
-# class QuestionSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Question
-#         fields = ('id', 'question_text', 'answer_one', 'answer_two', 'answer_three', 'answer_four','correct_answer', 'correct_answer_value')
+        fields = ("key", "value")
 
 
 class BookingSettlementReportSerializer(serializers.Serializer):
-    date = serializers.DateTimeField(input_formats=['%d/%m/%Y'])
+    date = serializers.DateTimeField(input_formats=["%d/%m/%Y"])
 
 
 class OracleSerializer(serializers.Serializer):
-    date = serializers.DateField(input_formats=['%d/%m/%Y','%Y-%m-%d'])
+    date = serializers.DateField(input_formats=["%d/%m/%Y", "%Y-%m-%d"])
     override = serializers.BooleanField(default=False)
 
-    
+
 class EmailUserROSerializerForReferral(serializers.ModelSerializer):
     name = serializers.SerializerMethodField()
-    telephone = serializers.CharField(source='phone_number')
-    mobile_phone = serializers.CharField(source='mobile_number')
+    telephone = serializers.CharField(source="phone_number")
+    mobile_phone = serializers.CharField(source="mobile_number")
 
     class Meta:
         model = EmailUserRO
         fields = (
-            'id',
-            'name',
-            'title',
-            'email',
-            'telephone',
-            'mobile_phone',
+            "id",
+            "name",
+            "title",
+            "email",
+            "telephone",
+            "mobile_phone",
         )
 
     def get_name(self, user):
@@ -112,5 +82,4 @@ class EmailUserSerializer(serializers.ModelSerializer):
         )
 
     def get_fullname(self, obj):
-        return "{} {}".format(obj.first_name, obj.last_name)
-
+        return f"{obj.first_name} {obj.last_name}"
