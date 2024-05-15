5fc2a53d6e04fa83333628200a3a37ea47d52313
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-05-15 16:25:25 +0800

    Remove bulk delete option from external contributors blacklist (to ensure save method is called for each delete).

--- boranga/components/users/admin.py
+++ boranga/components/users/admin.py
@@ -14,6 +14,12 @@ class ProfileAdmin(admin.ModelAdmin):
 class ExternalContributorBlacklistAdmin(admin.ModelAdmin):
     list_display = ("email", "reason", "datetime_created", "datetime_updated")
 
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if "delete_selected" in actions:
+            del actions["delete_selected"]
+        return actions
+
 
 admin.site.register(models.UserCategory, UserCategoryAdmin)
 admin.site.register(models.Profile, ProfileAdmin)
