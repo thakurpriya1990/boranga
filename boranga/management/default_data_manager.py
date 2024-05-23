c4fe5ff3ea07c37ccd2bfd711a2eddb881da32c3
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-05-23 15:54:32 +0800

    Add code to automatically populate the conervation change codes.

--- boranga/management/default_data_manager.py
+++ boranga/management/default_data_manager.py
@@ -3,6 +3,8 @@ import logging
 from django.conf import settings
 from ledger_api_client.managed_models import SystemGroup
 
+from boranga.components.conservation_status.models import ConservationChangeCode
+
 logger = logging.getLogger(__name__)
 
 
@@ -22,3 +24,18 @@ class DefaultDataManager:
         ):
             sg.systemgrouppermission_set.all().delete()
             sg.delete()
+
+        for conservation_change_code in settings.CONSERVATION_CHANGE_CODES:
+            try:
+                conservation_change_code_obj, created = (
+                    ConservationChangeCode.objects.get_or_create(
+                        code=conservation_change_code["code"],
+                        defaults={"label": conservation_change_code["label"]},
+                    )
+                )
+                if created:
+                    logger.info(
+                        f"Created ConservationChangeCode: {conservation_change_code_obj}"
+                    )
+            except Exception as e:
+                logger.error(f"{e}, ConservationChangeCode: {conservation_change_code}")
