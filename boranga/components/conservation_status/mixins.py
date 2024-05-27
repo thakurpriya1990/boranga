6db50a986b4956aa3a8b07eae616159d918b8e9f
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-05-27 10:55:00 +0800

    Don't think we actually use this anymore but fixing bug in case we do.

--- boranga/components/conservation_status/mixins.py
+++ boranga/components/conservation_status/mixins.py
@@ -4,9 +4,10 @@ from django.core.exceptions import PermissionDenied
 class ReferralOwnerMixin:
 
     def check_owner(self, user):
-        return self.get_object().referral == user
+        return self.get_object().referral == user.id
 
     def dispatch(self, request, *args, **kwargs):
         if not self.check_owner(request.user):
             raise PermissionDenied
+
         return super().dispatch(request, *args, **kwargs)
