ec5e4aa474bf51258859e2b989c798c363fdb664
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-06-04 11:43:55 +0800

    Add change_codes for GetSpeciesFilterDict.

--- boranga/components/species_and_communities/api.py
+++ boranga/components/species_and_communities/api.py
@@ -26,6 +26,7 @@ from rest_framework_datatables.pagination import DatatablesPageNumberPagination
 
 from boranga.components.conservation_status.models import (
     CommonwealthConservationList,
+    ConservationChangeCode,
     ConservationStatus,
     ConservationStatusUserAction,
     WALegislativeCategory,
@@ -451,6 +452,7 @@ class GetSpeciesFilterDict(views.APIView):
             "commonwealth_conservation_lists": CommonwealthConservationList.get_lists_dict(
                 group_type
             ),
+            "change_codes": ConservationChangeCode.get_filter_list(),
         }
         res_json = json.dumps(res_json)
         return HttpResponse(res_json, content_type="application/json")
