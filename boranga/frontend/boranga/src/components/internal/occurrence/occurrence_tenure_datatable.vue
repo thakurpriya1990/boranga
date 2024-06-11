f2eb7038a3957cc1113642ba8b9b86e333443958
Karsten Prehn <karsten.prehn@gmail.com>
2024-06-11 00:18:11 +0000

    Added occurrence_tenure_datatable vue file

---
+++ boranga/frontend/boranga/src/components/internal/occurrence/occurrence_tenure_datatable.vue
@@ -0,0 +1,45 @@
+<template id="occurrence_tenure_datatable_template">
+    <div>
+        Hello
+        <datatable
+            :id="datatable_id"
+            ref="occurrence_tenure_datatable"
+            :dtOptions="conservation_status_options"
+            :dtHeaders="conservation_status_headers"/>
+    </div>
+</template>
+
+<script>
+import {
+    api_endpoints,
+    helpers
+} from '@/utils/hooks';
+import datatable from '@/utils/vue/datatable.vue';
+import { v4 as uuid } from 'uuid';
+
+export default {
+    name: 'OccurrenceTenureDatatable',
+    components: {
+        datatable,
+    },
+    props: {
+        occurrenceTenure: {
+            type: Object,
+            required: true,
+        },
+    },
+    data: function () {
+        let vm = this;
+        return {
+            datatable_id: 'occurrence-tenure-datatable-' + uuid(),
+            conservation_status_headers: [
+                'Tenure Type',
+                'Tenure Status',
+                'Tenure Start Date',
+                'Tenure End Date',
+            ],
+            conservation_status_options: {},
+        };
+    },
+};
+</script>
