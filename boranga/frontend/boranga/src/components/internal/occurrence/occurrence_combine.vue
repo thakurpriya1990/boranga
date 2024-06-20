765cfefc304fad612035c77ad1ce4846bd749d7a
Ashley <ash.woodiss-field@dbca.wa.gov.au>
2024-06-20 08:04:07 +0000

    Added: OCC form section collapsibles and tabs to combine occurrence modal

--- boranga/frontend/boranga/src/components/internal/occurrence/occurrence_combine.vue
+++ boranga/frontend/boranga/src/components/internal/occurrence/occurrence_combine.vue
@@ -11,7 +11,7 @@
         >
 
         <!--OCC Selection Drop Down-->
-        <div class="row" id="occurrence_name_lookup_form_group_id">
+        <div class="row mb-3" id="occurrence_name_lookup_form_group_id">
             <label class="col-sm-3 control-label" for="occurrence_name_lookup">
                 Add Occurrence:
             </label>
@@ -26,15 +26,126 @@
             </div>
         </div>
         <!--OCC Selection Table-->
-        <div class="row">
+        <div class="row mb-3">
         <OccurrenceCombineSelect :selectedOccurrences="selectedOccurrences" :selectedOccurrenceIds="selectedOccurrenceIds" :mainOccurrenceId="main_occurrence_obj.id"/>
         </div>
 
         <!--OCC Tabs-->
-
-        <!--Main OCC Form-->
-
-        <!--Key Contacts Table-->
+        <div class="row mb-3">
+            <div class="col-md-12">
+                <ul id="pills-tab" class="nav nav-pills" role="tablist">
+                    <li class="nav-item">
+                        <a id="pills-occurrence-tab" class="nav-link active" data-bs-toggle="pill" :href="'#' + occurrenceBody"
+                            role="tab" :aria-controls="occurrenceBody" aria-selected="true" @click="tabClicked()">
+                            Occurrence
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="pills-location-tab" class="nav-link" data-bs-toggle="pill" :href="'#' + locationBody"
+                            role="tab" aria-selected="false" @click="tabClicked()">
+                            Location
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="pills-habitat-tab" class="nav-link" data-bs-toggle="pill" :href="'#' + habitatBody"
+                            role="tab" aria-selected="false" @click="tabClicked()">
+                            Habitat
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="pills-observation-tab" class="nav-link" data-bs-toggle="pill" :href="'#' + observationBody"
+                            role="tab" aria-selected="false" @click="tabClicked()">
+                            Observation
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="pills-documents-tab" class="nav-link" data-bs-toggle="pill" :href="'#' + documentBody"
+                            role="tab" aria-selected="false" @click="tabClicked()">
+                            Documents
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="pills-threats-tab" class="nav-link" data-bs-toggle="pill" :href="'#' + threatBody" role="tab"
+                            aria-selected="false" @click="tabClicked()">
+                            Threats
+                        </a>
+                    </li>
+                </ul>
+                <div id="pills-tabContent" class="tab-content">
+                    <div :id="occurrenceBody" class="tab-pane fade show active" role="tabpanel"
+                        aria-labelledby="pills-occurrence-tab">
+                        <!--Main OCC Form-->
+                        <FormSection :formCollapse="false" label="Occurrence" Index="occurrence">
+                            Main OCC Form
+                        </FormSection>
+                        <!--Key Contacts Table-->
+                        <FormSection :formCollapse="true" label="Key Contacts" Index="keyContacts">
+                            Key Contacts Table
+                        </FormSection>
+                    </div>
+                    <div :id="locationBody" class="tab-pane fade" role="tabpanel"
+                    aria-labelledby="pills-location-tab">
+                        <!--Location Form-->
+                        <FormSection :formCollapse="false" label="Location" Index="location">
+                            Location Form
+                        </FormSection>
+                    </div>
+                    <div :id="habitatBody" class="tab-pane fade" role="tabpanel" aria-labelledby="pills-habitat-tab">
+                        <!--Habitat Composition Form-->
+                        <FormSection :formCollapse="true" label="Habitat Composition" Index="habitat_composition">
+                            Habitat Composition Form
+                        </FormSection>
+                        <!--Habitat Condition Form-->
+                        <FormSection :formCollapse="true" label="Habitat Condition" Index="habitat_condition">
+                            Habitat Condition Form
+                        </FormSection>
+                        <!--Vegetation Structure Form-->
+                        <FormSection :formCollapse="true" label="Vegetation Structure" Index="vegetation_structure">
+                            Vegetation Structure Form
+                        </FormSection>
+                        <!--Fire History Form-->
+                        <FormSection :formCollapse="true" label="Fire History" Index="fire_history">
+                            Fire History Form
+                        </FormSection>
+                        <!--Associated Species Form-->
+                        <FormSection :formCollapse="true" label="Associated Species" Index="associated_species">
+                            Associated Species Form
+                        </FormSection>
+                    </div>
+                    <div :id="observationBody" class="tab-pane fade" role="tabpanel"
+                        aria-labelledby="pills-observation-tab">
+                        <!--Observation Details Form-->
+                        <FormSection :formCollapse="true" label="Observation Details" Index="observation_details">
+                            Observation Details Form
+                        </FormSection>
+                        <!--Animal Observation Form (fauna only)-->
+                        <FormSection :formCollapse="true" label="Animal Observation" Index="animal_observation">
+                            Animal Observation Form
+                        </FormSection>
+                        <!--Plant Count Form (flora only)-->
+                        <FormSection :formCollapse="true" label="Plant Count" Index="plant_count">
+                            <br/>Plant Count Form
+                        </FormSection>
+                        <!--Identification Form-->
+                        <FormSection :formCollapse="true" label="Identification" Index="identification">
+                            <br/>Identification Form
+                        </FormSection>
+                    </div>
+                    <div :id="documentBody" class="tab-pane fade" role="tabpanel" aria-labelledby="pills-documents-tab">
+                        <!--Documents Table-->
+                        <FormSection :formCollapse="false" label="Documents" Index="documents">
+                            Documents Table
+                        </FormSection>
+                    </div>
+                    <div :id="threatBody" class="tab-pane fade" role="tabpanel" aria-labelledby="pills-threats-tab">
+                        <!--Threats Table-->
+                        <FormSection :formCollapse="false" label="Threats" Index="threats">
+                            Threats Table
+                        </FormSection>
+                    </div>
+                </div>
+            </div>
+        </div>           
 
         </modal>
     </div>
@@ -42,6 +153,7 @@
 
 <script>
     import modal from '@vue-utils/bootstrap-modal.vue';
+    import FormSection from '@/components/forms/section_toggle.vue';
     import { helpers, api_endpoints } from "@/utils/hooks.js"
     import OccurrenceCombineSelect from './occurrence_combine_selection.vue'
     export default {
@@ -55,9 +167,18 @@
         components: {
             modal,
             OccurrenceCombineSelect,
+            FormSection,
         },
         data: function () {
+            let vm = this;
             return {
+                reloadcount: 0,
+                locationBody: 'locationBody' + vm._uid,
+                habitatBody: 'habitatBody' + vm._uid,
+                observationBody: 'observationBody' + vm._uid,
+                threatBody: 'threatBody' + vm._uid,
+                documentBody: 'documentBody' + vm._uid,
+                occurrenceBody: 'occurrenceBody' + vm._uid,
                 isModalOpen: false,
                 selectedOccurrences: [this.main_occurrence_obj],
                 selectedOccurrenceIds: [this.main_occurrence_obj.id],
@@ -72,6 +193,9 @@
                 this.errorString = '';
                 this.isModalOpen = false;
                 $('.has-error').removeClass('has-error');
+            },
+            tabClicked: function (param) {
+                this.reloadcount = this.reloadcount + 1;
             },
             addOccurrence: function () {
                 let vm = this;
