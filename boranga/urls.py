8aeb56df7e673502dca6e0820df2dbde3d19f3cf
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-06-20 15:30:02 +0800

    Bug fix: Fix conservation status referral url.

--- boranga/urls.py
+++ boranga/urls.py
@@ -374,7 +374,7 @@ urlpatterns = [
         name="internal-conservation-status-dashboard",
     ),
     url(
-        r"^internal/conservation_status/(?P<cs_proposal_pk>\d+)/cs_referral/(?P<referral_pk>\d+)/$",
+        r"^internal/conservation_status/(?P<cs_proposal_pk>\d+)/referral/(?P<referral_pk>\d+)/$",
         views.ConservationStatusReferralView.as_view(),
         name="internal-conservation-status-referral-detail",
     ),
