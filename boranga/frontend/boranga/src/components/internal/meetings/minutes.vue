ed678fc77e8644ee81f4f66a5f12bea34f44935b
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-06-24 15:38:04 +0800

    Use new property can_user_add_minutes to allow cs approvers to be able to add minutes to a meeting in any status (even completed).

--- boranga/frontend/boranga/src/components/internal/meetings/minutes.vue
+++ boranga/frontend/boranga/src/components/internal/meetings/minutes.vue
@@ -2,7 +2,7 @@
     <div id="minutes">
         <FormSection :formCollapse="false" label="Minutes" :Index="minutesBody">
             <form class="form-horizontal" action="index.html" method="post">
-                <div v-if="meeting_obj.can_user_edit" class="col-sm-12">
+                <div v-if="meeting_obj.can_user_add_minutes" class="col-sm-12">
                     <div class="text-end">
                         <button type="button" class="btn btn-primary mb-2 " @click.prevent="newDocument">
                             <i class="fa-solid fa-circle-plus"></i>
@@ -44,6 +44,10 @@ export default {
         meeting_obj: {
             type: Object,
             required: true
+        },
+        is_internal: {
+            type: Boolean,
+            default: false
         },
     },
     data: function () {
