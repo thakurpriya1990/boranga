432f84ff5a4cf8c17449b5b3c37a58ce06f954b3
Karsten Prehn <karsten.prehn@gmail.com>
2024-06-28 07:44:22 +0000

    Added occurrance report fauna and community sql scripts

--- docs/sql-scripts/OccurrenceReportFlora.sql
+++ docs/sql-scripts/OccurrenceReportFlora.sql
@@ -24,7 +24,8 @@ ocr AS (
         community_id,
         observation_date,
         reported_date,
-        lodgement_date
+        lodgement_date,
+        site
     FROM
         boranga_occurrencereport,
         constants
@@ -38,12 +39,30 @@ occ AS (
     FROM
         boranga_occurrence
 ),
+loc AS (
+    SELECT
+        l.id,
+        l.occurrence_report_id,
+        r.name AS region_name,
+        d.name AS district_name,
+        d.code AS district_code,
+        l.locality,
+        l.location_description,
+        l.boundary_description,
+        cs.name AS coordination_source_name,
+        la.name AS location_accuracy_name
+    FROM
+        boranga_ocrlocation l LEFT JOIN boranga_region r ON l.region_id = r.id
+        LEFT JOIN boranga_district d ON l.district_id = d.id
+        LEFT JOIN boranga_coordinationsource cs ON l.coordination_source_id = cs.id
+        LEFT JOIN boranga_locationaccuracy la ON l.location_accuracy_id = la.id
+),
 geom AS (
     SELECT
         id,
         CASE
             ST_GeometryType(geometry)
-            WHEN 'ST_Point' THEN ST_Buffer(geometry, 0.00001) -- 0.00001 deg ~= 1.1m
+            WHEN 'ST_Point' THEN ST_Buffer(geometry, 0.00001)
             ELSE geometry
         END AS geometry,
         ST_GeometryType(geometry) AS geometry_type,
@@ -68,17 +87,21 @@ community AS (
         boranga_community
 )
 SELECT
-    ocr.occurrence_report_number,
-    occ.occurrence_number,
+    ocr.occurrence_report_number AS ocr_number,
+    occ.occurrence_number AS occ_number,
     gt.name AS group_type,
     geom.geometry AS geometry,
-    geom.geometry_type AS geometry_type,
-    geom.data_type AS geometry_data_type,
-    species.species_number AS species_number,
-    community.community_number AS community_number,
-    ocr.observation_date,
-    ocr.reported_date,
-    ocr.lodgement_date
+    geom.geometry_type AS geom_type,
+    geom.data_type AS g_data_type,
+    species.species_number AS species_nr,
+    community.community_number AS commnty_nr,
+    ocr.observation_date AS obs_date,
+    ocr.reported_date AS rep_date,
+    ocr.lodgement_date AS lodg_date,
+    ocr.site AS site,
+    loc.locality AS locality,
+    loc.location_description AS loc_desc,
+    loc.boundary_description AS bound_desc
 FROM
     ocr
     RIGHT JOIN geom ON ocr.id = geom.occurrence_report_id
@@ -86,5 +109,6 @@ FROM
     LEFT JOIN occ ON ocr.occurrence_id = occ.id
     LEFT JOIN species ON ocr.species_id = species.id
     LEFT JOIN community ON ocr.community_id = community.id
+    LEFT JOIN loc ON ocr.id = loc.occurrence_report_id
 WHERE
     ocr.group_type_id = gt.id;
