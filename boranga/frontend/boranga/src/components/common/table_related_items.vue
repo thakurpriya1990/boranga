6d9401a91d9ee1cd3bf5bceb167bbae5bcff2bcb
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-07-01 10:40:43 +0800

    Clean up.

--- boranga/frontend/boranga/src/components/common/table_related_items.vue
+++ boranga/frontend/boranga/src/components/common/table_related_items.vue
@@ -1,14 +1,15 @@
 <template lang="html">
     <div id="related_items">
         <FormSection :formCollapse="false" label="Related Items" Index="related_items">
-            <CollapsibleFilters component_title="Filters" ref="collapsible_filters" @created="collapsible_component_mounted" class="mb-2">
+            <CollapsibleFilters component_title="Filters" ref="collapsible_filters"
+                @created="collapsible_component_mounted" class="mb-2">
                 <div class="row">
                     <div class="col-md-3">
                         <div class="form-group">
                             <label for="">Related Type:</label>
                             <select class="form-select" v-model="filterRelatedType">
                                 <option value="all">All</option>
-                                <option v-for="option in related_type_filter_list" :value="option[0]">{{option[1]}}
+                                <option v-for="option in related_type_filter_list" :value="option[0]">{{ option[1] }}
                                 </option>
                             </select>
                         </div>
@@ -16,12 +17,8 @@
                 </div>
             </CollapsibleFilters>
             <div>
-                <datatable
-                    ref="related_items_datatable"
-                    :id="datatable_id"
-                    :dtOptions="datatable_options"
-                    :dtHeaders="datatable_headers"
-                />
+                <datatable ref="related_items_datatable" :id="datatable_id" :dtOptions="datatable_options"
+                    :dtHeaders="datatable_headers" />
             </div>
         </FormSection>
     </div>
@@ -41,11 +38,16 @@ export default {
         CollapsibleFilters,
     },
     props: {
-        ajax_url: '',
-        filter_list_url: '',
+        ajax_url: {
+            type: String,
+            required: true,
+        },
+        filter_list_url: {
+            type: String,
+            required: true,
+        },
     },
     data() {
-        let vm = this;
         return {
             datatable_id: uuid(),
             related_type_filter_list: [],
@@ -53,64 +55,51 @@ export default {
         }
     },
     computed: {
-        column_lodgement_number: function(){
+        column_lodgement_number: function () {
             return {
                 data: 'identifier',
-                //name: 'lodgement_number',
                 orderable: false,
                 searchable: false,
                 visible: true,
-                //'render': function(row, type, full){
-                //}
             }
         },
-        column_type: function(){
+        column_type: function () {
             return {
                 data: 'model_name',
-                //name: 'type',
                 orderable: false,
                 searchable: false,
                 visible: true,
-                //'render': function(row, type, full){
-                //}
             }
         },
-        column_description: function(){
+        column_description: function () {
             return {
                 data: 'descriptor',
-                //name: 'descriptor',
                 orderable: false,
                 searchable: false,
                 visible: true,
-                'render': function(value, type){
+                'render': function (value, type) {
                     let result = helpers.dtPopover(value, 30, 'hover');
-                    return type=='export' ? value : result;
+                    return type == 'export' ? value : result;
                 },
             }
         },
-        column_status: function(){
+        column_status: function () {
             return {
                 data: 'status',
-                //name: 'type',
                 orderable: false,
                 searchable: false,
                 visible: true,
-                //'render': function(row, type, full){
-                //}
             }
         },
-        column_action: function(){
+        column_action: function () {
             return {
                 data: 'action_url',
-                //name: 'action',
                 orderable: false,
                 searchable: false,
                 visible: true,
-                //'render': function(row, type, full){
-                //}
             }
         },
-        datatable_options: function(){
+        datatable_options: function () {
             let vm = this
             let columns = [
                 vm.column_lodgement_number,
@@ -125,30 +114,24 @@ export default {
                     processing: constants.DATATABLE_PROCESSING_HTML
                 },
                 responsive: true,
-                //serverSide: true,
                 searching: true,
                 ordering: true,
                 order: [[0, 'desc']],
                 ajax: {
-                    //"url": '/api/proposal/' + vm.proposal.id + '/get_related_items/',
                     "url": vm.ajax_url,
                     "dataSrc": "",
-                    // adding extra GET params for Custom filtering
-                    "data": function ( d ) {
+                    "data": function (d) {
                         d.related_filter_type = vm.filterRelatedType
                     }
                 },
                 dom: 'lBfrtip',
-                buttons:[ ],
+                buttons: [],
                 columns: columns,
                 processing: true,
-                initComplete: function(settings, json) {
-                },
             }
         },
-        datatable_headers: function(){
+        datatable_headers: function () {
             return [
-                //'id',
                 'Number',
                 'Type',
                 'Description',
@@ -156,41 +139,41 @@ export default {
                 'Action',
             ]
         },
-        filterApplied: function(){
-            if(this.filterRelatedType === 'all'){
+        filterApplied: function () {
+            if (this.filterRelatedType === 'all') {
                 return false
             } else {
                 return true
             }
         },
     },
-    watch:{
-        filterRelatedType: function(){
+    watch: {
+        filterRelatedType: function () {
             let vm = this;
-            vm.$refs.related_items_datatable.vmDataTable.ajax.reload(); // This calls ajax() backend call.
+            vm.$refs.related_items_datatable.vmDataTable.ajax.reload();
         },
-        filterApplied: function(){
-            if (this.$refs.collapsible_filters){
+        filterApplied: function () {
+            if (this.$refs.collapsible_filters) {
                 // Collapsible component exists
                 this.$refs.collapsible_filters.show_warning_icon(this.filterApplied)
             }
         },
     },
-    methods:{
-        collapsible_component_mounted: function(){
+    methods: {
+        collapsible_component_mounted: function () {
             this.$refs.collapsible_filters.show_warning_icon(this.filterApplied)
         },
-        fetchFilterList: function(){
+        fetchFilterList: function () {
             let vm = this;
             //large FilterList of Species Values object
             vm.$http.get(vm.filter_list_url).then((response) => {
                 vm.related_type_filter_list = response.body;
-            },(error) => {
+            }, (error) => {
                 console.log(error);
             })
         },
     },
-    mounted: function(){
+    mounted: function () {
         let vm = this;
         vm.fetchFilterList();
     }
