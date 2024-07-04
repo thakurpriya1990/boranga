4581616b9d4cde10fa240a50de954668a29dcdea
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-07-04 16:39:29 +0800

    Enable USE_X_FORWARDED_HOST and add SECURE_PROXY_SSL_HEADER as currently even if the request is https, django is returning filefield urls with http (non-secure) which causes a browser warning. Will require some changes to nginx config to support.

--- boranga/settings.py
+++ boranga/settings.py
@@ -331,6 +331,10 @@ GIS_SERVER_URL = env(
 # Proxy prefix for basic authentication
 BASIC_AUTH_PROXY_PREFIX = env("BASIC_AUTH_PROXY_PREFIX", "kb-proxy/")
 
+# Ensure that if the request is https then urls generated for file fields are also https
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 # ---------- Identifier fields for logging ----------
 
 """ Fields that the logging functions will check for on the instance
