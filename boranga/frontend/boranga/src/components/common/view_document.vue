ad1c38f4d7446478eead93776fedd16fffb454b9
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-07-17 09:41:30 +0800

    New view document modal component.:

---
+++ boranga/frontend/boranga/src/components/common/view_document.vue
@@ -0,0 +1,77 @@
+<template>
+    <div id="view-document">
+        <modal transition="modal fade" @cancel="close()" :showOK="false" cancelText="Close" :title="title" large>
+            <form v-if="document" class="form-horizontal px-3 pt-2" name="documentForm">
+                <div class="row border-bottom mb-2 pb-2">
+                    <label for="" class="col-sm-3 col-form-label fw-bold">Document Number</label>
+                    <div class="col form-control-plaintext">{{ document.document_number }}</div>
+                </div>
+                <div v-if="document.document_category_name" class="row border-bottom mb-2 pb-2">
+                    <label for="" class="col-sm-3 col-form-label fw-bold">Category</label>
+                    <div class="col form-control-plaintext">{{ document.document_category_name }}</div>
+                </div>
+                <div v-if="document.document_sub_category_name" class="row border-bottom mb-2 pb-2">
+                    <label for="" class="col-sm-3 col-form-label fw-bold">Sub Category</label>
+                    <div class="col form-control-plaintext">{{ document.document_sub_category_name }}</div>
+                </div>
+                <div class="row border-bottom mb-2 pb-2">
+                    <label for="" class="col-sm-3 col-form-label fw-bold">Description</label>
+                    <div class="col form-control-plaintext">{{ document.description }}</div>
+                </div>
+                <div class="row border-bottom mb-2 pb-2">
+                    <label for="" class="col-sm-3 col-form-label fw-bold">Document</label>
+                    <div class="col form-control-plaintext"><a :href="document._file" target="_blank">{{ document.name
+                            }}</a></div>
+                </div>
+                <div class="row pb-3" :class="is_internal ? 'mb-3 border-bottom' : ''">
+                    <label for="" class="col-sm-3 col-form-label fw-bold">Date Uploaded</label>
+                    <div class="col form-control-plaintext">{{ new Date(document.uploaded_date).toLocaleDateString() }}
+                        at {{ new Date(document.uploaded_date).toLocaleTimeString() }}</div>
+                </div>
+                <div v-if="is_internal" class="row border-bottom mb-2 pb-2">
+                    <label for="" class="col-sm-3 col-form-label fw-bold">Visible</label>
+                    <div class="col form-control-plaintext">{{ document.visible ? 'Yes' : 'No' }}</div>
+                </div>
+                <div v-if="is_internal" class="row pb-3">
+                    <label for="" class="col-sm-3 col-form-label fw-bold">Can Submitter Access</label>
+                    <div class="col form-control-plaintext">{{ document.can_submitter_access ? 'Yes' : 'No' }}</div>
+                </div>
+            </form>
+        </modal>
+    </div>
+</template>
+
+<script>
+import modal from '@vue-utils/bootstrap-modal.vue'
+
+export default {
+    name: 'ViewDocument',
+    components: {
+        modal,
+    },
+    props: {
+        is_internal: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            document: null,
+            isModalOpen: false,
+        }
+    },
+    computed: {
+        title() {
+            let documentNumber = this.document ? this.document.document_number : ''
+            return `View Document ${documentNumber}`
+        }
+    },
+    methods: {
+        close() {
+            this.document = null;
+            this.isModalOpen = false
+        },
+    }
+}
+</script>
