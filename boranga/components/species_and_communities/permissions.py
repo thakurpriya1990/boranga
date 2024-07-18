07ccb82b4c324e6b36eb4ccb5bccc2267fc9b655
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-07-18 16:54:11 +0800

    Implment basic SpeciesCommunitiesPermission.

---
+++ boranga/components/species_and_communities/permissions.py
@@ -0,0 +1,36 @@
+import logging
+
+from rest_framework import permissions
+from rest_framework.permissions import BasePermission
+
+from boranga.helpers import (
+    is_conservation_status_approver,
+    is_conservation_status_assessor,
+    is_occurrence_approver,
+    is_occurrence_assessor,
+    is_readonly_user,
+    is_species_communities_approver,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class SpeciesCommunitiesPermission(BasePermission):
+    def has_permission(self, request, view):
+        if hasattr(view, "action") and view.action == "create":
+            return is_species_communities_approver(request)
+
+        return (
+            is_readonly_user(request)
+            or is_conservation_status_assessor(request)
+            or is_conservation_status_approver(request)
+            or is_species_communities_approver(request)
+            or is_occurrence_assessor(request)
+            or is_occurrence_approver(request)
+        )
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return is_species_communities_approver(request)
