7eb4c53c1bc2eac55971b75704b72cf4bd063b50
Karsten Prehn <karsten.prehn@gmail.com>
2024-07-23 01:28:02 +0000

    Update SQl files purpose and vesting to code instead of label

--- docs/sql-scripts/OccurrenceFauna.sql
+++ docs/sql-scripts/OccurrenceFauna.sql
@@ -143,8 +143,8 @@ geom AS (
                     ',',
                     otc.tenure_area_id,
                     otc.status,
-                    coalesce(p.label, ''),
-                    coalesce(v.label, '')
+                    coalesce(p.code, ''),
+                    coalesce(v.code, '')
                 )
             ),
             '; '
