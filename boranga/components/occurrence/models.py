64a7559a4f93ec147fde5bb434cbeeb23827a78d
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-07-24 16:46:27 +0800

    Order sample type by group_type and then name.

--- boranga/components/occurrence/models.py
+++ boranga/components/occurrence/models.py
@@ -2785,7 +2785,7 @@ class SampleType(models.Model):
 
     class Meta:
         app_label = "boranga"
-        ordering = ["name"]
+        ordering = ["group_type", "name"]
 
     def __str__(self):
         return str(self.name)
