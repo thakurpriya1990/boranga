3c564aca77970eb4d84cd83673ec35f7771f6fb5
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-07-26 16:42:34 +0800

    Update to use match new docker build.

--- startup.sh
+++ startup.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-eval $(grep -v '^#' /container-config/.cronenv | xargs -d "\n" -I {} echo export \"{}\" )
-env
-whoami
-if [ -z "$SUDO_OIM_ENCRYPTED_PASSWORD" ]; then
-    echo "SUDO_OIM_ENCRYPTED_PASSWORD Ignored":
-else
-    # create a new password.
-    echo "SUDO_OIM_ENCRYPTED_PASSWORD Preparing";
-    SUDO_OIM_ENCRYPTED_PASSWORD_SIZE="$(echo $SUDO_OIM_ENCRYPTED_PASSWORD | wc -m)"
-    if [ "$SUDO_OIM_ENCRYPTED_PASSWORD_SIZE" -gt "10" ]; then
-        usermod -p "$SUDO_OIM_ENCRYPTED_PASSWORD" oim
-        echo "SUDO_OIM_ENCRYPTED_PASSWORD Updated";
-    fi
-fi
-
+cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1 > /app/git_hash
 
 if [ $ENABLE_CRON == "True" ];
-then
-    echo "Starting Cron"
-    service cron start &
+    then
+    echo "Starting Python Cron"
+    python /bin/scheduler.py /app/python-cron /app/logs/python-cron.log &
     status=$?
     if [ $status -ne 0 ]; then
         echo "Failed to start cron: $status"
         exit $status
     fi
+fi
+
+if [ $ENABLE_WEB == "True" ];
+    then
+    echo "Starting Gunicorn"
+    /app/venv/bin/gunicorn  boranga.wsgi --bind :8080 --config /app/gunicorn.ini
+    status=$?
+    if [ $status -ne 0 ]; then
+    echo "Failed to start gunicorn: $status"
+    exit $status
+    fi
 else
-   echo "ENABLE_CRON environment variable not set to True, cron is not starting."
+   echo "ENABLE_WEB environment vairable not set to True, web server is not starting."
+   /bin/bash
 fi
