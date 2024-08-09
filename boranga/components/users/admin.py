176a8a439675b15b430c1adb63ea0b3728654ae1
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-08-09 10:38:01 +0800

    Add custom validation to ExternalContributorBlacklistAdmin. To prevent adding emails that don't have an associated ledger account.

--- boranga/components/users/admin.py
+++ boranga/components/users/admin.py
@@ -1,4 +1,7 @@
+from django import forms
 from django.contrib import admin
+from django.forms import ValidationError
+from ledger_api_client.ledger_models import EmailUserRO as EmailUser
 
 from boranga.admin import ArchivableModelAdminMixin, DeleteProtectedModelAdmin
 from boranga.components.users import models
@@ -12,8 +15,30 @@ class SubmitterInformationAdmin(admin.ModelAdmin):
     list_display = ("name", "organisation", "position", "submitter_category")
 
 
+class ExternalContributorBlacklistForm(forms.ModelForm):
+    class Meta:
+        model = models.ExternalContributorBlacklist
+        fields = "__all__"
+
+    def clean(self):
+        email = self.cleaned_data["email"]
+
+        try:
+            EmailUser.objects.get(email=email)
+        except EmailUser.DoesNotExist:
+            raise ValidationError(
+                f"EmailUser with email {email} does not exist in ledger. Cannot blacklist."
+            )
+
+        if models.ExternalContributorBlacklist.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already blacklisted.")
+
+        return super().clean()
+
+
 class ExternalContributorBlacklistAdmin(admin.ModelAdmin):
     list_display = ("email", "reason", "datetime_created", "datetime_updated")
+    form = ExternalContributorBlacklistForm
 
     def get_actions(self, request):
         actions = super().get_actions(request)
