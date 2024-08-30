33aec43f970829f43730c450af1bcab6ad52e1b7
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-08-30 14:21:54 +0800

    Do the preprocessing row count every 2 minutes. Process the next item in queue every 5 minutes.

--- boranga/cron.py
+++ boranga/cron.py
@@ -16,3 +16,23 @@ class CronJobFetchNomosTaxonDataDaily(django_cron.CronJobBase):
         log.info("Fetch Nomos Taxon Data cron job triggered, running...")
         management.call_command("fetch_nomos_blob_data")
         return "Job Completed Successfully"
+
+
+class CronJobOCRPreProcessBulkImportTasks(django_cron.CronJobBase):
+    schedule = django_cron.Schedule(run_on_days=[0, 1, 2, 3, 4, 5, 6], run_every_mins=2)
+    code = "boranga.ocr_pre_process_bulk_import_tasks"
+
+    def do(self) -> None:
+        log.info("OCR Pre-process Bulk Import Tasks cron job triggered, running...")
+        management.call_command("ocr_pre_process_bulk_import_tasks")
+        return "Job Completed Successfully"
+
+
+class CronJobOCRProcessBulkImportQueue(django_cron.CronJobBase):
+    schedule = django_cron.Schedule(run_on_days=[0, 1, 2, 3, 4, 5, 6], run_every_mins=5)
+    code = "boranga.ocr_process_bulk_import_queue"
+
+    def do(self) -> None:
+        log.info("OCR Process Bulk Import Tasks cron job triggered, running...")
+        management.call_command("ocr_process_bulk_import_queue")
+        return "Job Completed Successfully"
