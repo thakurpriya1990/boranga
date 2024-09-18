7e8534100c55a18572e41b9ae5bc2b6b48148d88
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-09-18 09:23:55 +0800

    Remove unused field recurrence_schedule and all references.

--- boranga/components/conservation_status/models.py
+++ boranga/components/conservation_status/models.py
@@ -545,7 +545,6 @@ class ConservationStatus(SubmitterInformationModelMixin, RevisionedMixin):
     comment = models.CharField(max_length=512, blank=True, null=True)
     review_due_date = models.DateField(null=True, blank=True)
     reviewed_by = models.IntegerField(null=True)  # EmailUserRO
-    recurrence_schedule = models.IntegerField(null=True, blank=True)
     effective_from = models.DateField(null=True, blank=True)
     effective_to = models.DateField(null=True, blank=True)
     submitter = models.IntegerField(null=True)  # EmailUserRO
