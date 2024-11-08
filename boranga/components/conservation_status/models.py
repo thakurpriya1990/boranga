115cbe979c4333d2bb506691fa310255d4f251b8
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-11-08 09:24:09 +0800

    Remove unused processing statuses. Add in new required processing statuses. Add in new filtering groups for processing statuses.

--- boranga/components/conservation_status/models.py
+++ boranga/components/conservation_status/models.py
@@ -381,38 +381,60 @@ class ConservationStatus(SubmitterInformationModelMixin, RevisionedMixin):
         "closed",
     ]
 
-    PROCESSING_STATUS_TEMP = "temp"
-    PROCESSING_STATUS_DRAFT = "draft"
-    PROCESSING_STATUS_WITH_ASSESSOR = "with_assessor"
-    PROCESSING_STATUS_WITH_REFERRAL = "with_referral"
-    PROCESSING_STATUS_WITH_APPROVER = "with_approver"
-    PROCESSING_STATUS_READY_FOR_AGENDA = "ready_for_agenda"
-    PROCESSING_STATUS_AWAITING_APPLICANT_RESPONSE = "awaiting_applicant_respone"
-    PROCESSING_STATUS_AWAITING_ASSESSOR_RESPONSE = "awaiting_assessor_response"
-    PROCESSING_STATUS_AWAITING_RESPONSES = "awaiting_responses"
     PROCESSING_STATUS_APPROVED = "approved"
+    PROCESSING_STATUS_CLOSED = "closed"
     PROCESSING_STATUS_DECLINED = "declined"
-    PROCESSING_STATUS_UNLOCKED = "unlocked"
+    PROCESSING_STATUS_DEFERRED = "deferred"
+    PROCESSING_STATUS_DELISTED = "delisted"
     PROCESSING_STATUS_DISCARDED = "discarded"
     PROCESSING_STATUS_DISCARDED_INTERNALLY = "discarded_internally"
-    PROCESSING_STATUS_CLOSED = "closed"
-    PROCESSING_STATUS_DELISTED = "delisted"
+    PROCESSING_STATUS_DRAFT = "draft"
+    PROCESSING_STATUS_ON_AGENDA = "on_agenda"
+    PROCESSING_STATUS_PROPOSED_FOR_AGENDA = "proposed_for_agenda"
+    PROCESSING_STATUS_READY_FOR_AGENDA = "ready_for_agenda"
+    PROCESSING_STATUS_UNLOCKED = "unlocked"
+    PROCESSING_STATUS_WITH_APPROVER = "with_approver"
+    PROCESSING_STATUS_WITH_ASSESSOR = "with_assessor"
+    PROCESSING_STATUS_WITH_REFERRAL = "with_referral"
+
     PROCESSING_STATUS_CHOICES = (
         (PROCESSING_STATUS_DRAFT, "Draft"),
+        (PROCESSING_STATUS_DISCARDED, "Discarded"),
         (PROCESSING_STATUS_WITH_ASSESSOR, "With Assessor"),
         (PROCESSING_STATUS_WITH_REFERRAL, "With Referral"),
-        (PROCESSING_STATUS_WITH_APPROVER, "Proposed DeListed"),
+        (PROCESSING_STATUS_DEFERRED, "Deferred"),
+        (PROCESSING_STATUS_PROPOSED_FOR_AGENDA, "Proposed For Agenda"),
         (PROCESSING_STATUS_READY_FOR_AGENDA, "Ready For Agenda"),
-        (PROCESSING_STATUS_AWAITING_APPLICANT_RESPONSE, "Awaiting Applicant Response"),
-        (PROCESSING_STATUS_AWAITING_ASSESSOR_RESPONSE, "Awaiting Assessor Response"),
-        (PROCESSING_STATUS_AWAITING_RESPONSES, "Awaiting Responses"),
+        (PROCESSING_STATUS_ON_AGENDA, "On Agenda"),
+        (PROCESSING_STATUS_WITH_APPROVER, "Proposed DeListed"),
         (PROCESSING_STATUS_APPROVED, "Approved"),
         (PROCESSING_STATUS_DECLINED, "Declined"),
-        (PROCESSING_STATUS_UNLOCKED, "Unlocked"),
-        (PROCESSING_STATUS_DISCARDED, "Discarded"),
         (PROCESSING_STATUS_DELISTED, "DeListed"),
         (PROCESSING_STATUS_CLOSED, "Closed"),
+        (PROCESSING_STATUS_UNLOCKED, "Unlocked"),
     )
+
+    # The following tuples of statuses are only user for filtering purposes
+    PROCESSING_STATUSES_WAITING_ASSESSOR_ACTION = (
+        PROCESSING_STATUS_WITH_ASSESSOR,
+        PROCESSING_STATUS_WITH_REFERRAL,
+        PROCESSING_STATUS_DEFERRED,
+    )
+
+    PROCESSING_STATUSES_WAITING_APPROVER_ACTION = (
+        PROCESSING_STATUS_PROPOSED_FOR_AGENDA,
+        PROCESSING_STATUS_READY_FOR_AGENDA,
+        PROCESSING_STATUS_ON_AGENDA,
+        PROCESSING_STATUS_DEFERRED,
+        PROCESSING_STATUS_WITH_APPROVER,
+    )
+
+    PROCESSING_STATUSES_INACTIVE = (
+        PROCESSING_STATUS_DECLINED,
+        PROCESSING_STATUS_CLOSED,
+        PROCESSING_STATUS_DELISTED,
+    )
+
     # This status is used as a front end filter but shouldn't be used in most cases
     # So it is deliberately not included in PROCESSING_STATUS_CHOICES
     PROCESSING_STATUS_DISCARDED_BY_ME = "discarded_by_me"
@@ -676,7 +698,6 @@ class ConservationStatus(SubmitterInformationModelMixin, RevisionedMixin):
             ConservationStatus.PROCESSING_STATUS_DRAFT,
             ConservationStatus.PROCESSING_STATUS_APPROVED,
             ConservationStatus.PROCESSING_STATUS_DECLINED,
-            ConservationStatus.PROCESSING_STATUS_TEMP,
             ConservationStatus.PROCESSING_STATUS_DISCARDED,
             ConservationStatus.PROCESSING_STATUS_CLOSED,
             ConservationStatus.PROCESSING_STATUS_DELISTED,
