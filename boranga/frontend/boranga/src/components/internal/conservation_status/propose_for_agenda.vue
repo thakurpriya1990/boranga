9aca5ffae3aaf0cb0543945b5a2c77eae3df30c3
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-11-11 13:52:07 +0800

    Rename propose ready for agenda component to propose for agenda.

--- boranga/frontend/boranga/src/components/internal/conservation_status/propose_for_agenda.vue
+++ boranga/frontend/boranga/src/components/internal/conservation_status/propose_for_agenda.vue
@@ -1,11 +1,11 @@
 <template lang="html">
-    <div id="internal-conservation-status-proposal-propose-ready-for-agenda">
-        <modal id="myModal" transition="modal fade" @ok="ok()" @cancel="close()"
+    <div id="internal-conservation-status-proposal-propose-for-agenda">
+        <modal id="propose-for-agenda-modal" transition="modal fade" @ok="ok()" @cancel="close()"
             :title="`Propose CS${conservation_status_id} for Agenda`" okText="Propose for Agenda" large>
             <div class="container-fluid">
                 <div class="row">
-                    <form class="form-horizontal needs-validation" id="propose-ready-for-agenda-form"
-                        name="propose-ready-for-agenda-form" novalidate>
+                    <form class="form-horizontal needs-validation" id="propose-for-agenda-form"
+                        name="propose-for-agenda-form" novalidate>
                         <alert v-if="errors" type="danger"><strong>{{ errors }}</strong></alert>
                         <div class="col-sm-12">
                             <div class="row mb-3">
@@ -35,7 +35,7 @@ import alert from '@vue-utils/alert.vue'
 import { helpers, api_endpoints } from "@/utils/hooks.js"
 
 export default {
-    name: 'ProposeReadyForAgenda',
+    name: 'ProposeForAgenda',
     components: {
         modal,
         alert,
@@ -65,12 +65,12 @@ export default {
     methods: {
         ok: function () {
             let vm = this;
-            var form = document.getElementById('propose-ready-for-agenda-form');
+            var form = document.getElementById('propose-for-agenda-form');
             if (form.checkValidity()) {
-                vm.proposeReadyForAgenda();
+                vm.proposeForAgenda();
             } else {
                 form.classList.add('was-validated');
-                $('#propose-ready-for-agenda-form').find(':invalid').first().focus();
+                $('#propose-for-agenda-form').find(':invalid').first().focus();
             }
             return false;
         },
@@ -79,10 +79,10 @@ export default {
             this.errors = null;
             this.isModalOpen = false;
         },
-        proposeReadyForAgenda: function () {
+        proposeForAgenda: function () {
             let vm = this;
-            vm.$http.post(helpers.add_endpoint_json(api_endpoints.conservation_status, vm.conservation_status_id + '/proposed_ready_for_agenda'), { 'assessor_comment': vm.assessor_comment }).then((response) => {
-                vm.$router.push({ path: '/internal/conservation-status/' }); //Navigate to dashboard page after Propose issue.
+            vm.$http.patch(helpers.add_endpoint_json(api_endpoints.conservation_status, vm.conservation_status_id + '/proposed_for_agenda'), { 'assessor_comment': vm.assessor_comment }).then((response) => {
+                vm.$router.push({ path: '/internal/conservation-status/' });
             }, (error) => {
                 vm.errors = true;
                 vm.errorString = helpers.apiVueResourceError(error);
