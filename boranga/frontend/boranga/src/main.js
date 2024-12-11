58b5c800efacc6782ec6a4143dff9ab92381d150
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2024-12-11 11:46:06 +0800

    Update main.js to work with vite.

--- boranga/frontend/boranga/src/main.js
+++ boranga/frontend/boranga/src/main.js
@@ -1,28 +1,39 @@
+import 'vite/modulepreload-polyfill';
+
 import { createApp } from 'vue';
-import App from './App'
-import router from './router'
-import helpers from '@/utils/helpers'
+import App from './App.vue';
+import router from './router';
+import helpers from '@/utils/helpers';
 import { extendMoment } from 'moment-range';
+
+import _ from 'lodash';
+window._ = _;
+import $ from 'jquery';
+import select2 from 'select2';
+window.$ = $;
+import moment from 'moment';
+window.moment = moment;
+import swal from 'sweetalert2';
+window.swal = swal;
+select2();
 
 import 'datatables.net-bs5';
 import 'datatables.net-buttons-bs5';
 import 'datatables.net-responsive-bs5';
 import 'datatables.net-buttons/js/dataTables.buttons.js';
-const jsZip = require('jszip');
-window.JSZip = jsZip;
+import JSZip from 'jszip';
+window.JSZip = JSZip;
 import 'datatables.net-buttons/js/buttons.html5.js';
 import 'select2';
 import 'jquery-validation';
 
 import 'sweetalert2/dist/sweetalert2.css';
-import '@/../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
 import 'select2/dist/css/select2.min.css';
 import 'select2-bootstrap-5-theme/dist/select2-bootstrap-5-theme.min.css';
 import '@/../node_modules/datatables.net-bs5/css/dataTables.bootstrap5.min.css';
 import '@/../node_modules/datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css';
-
-require('@/../node_modules/@fortawesome/fontawesome-free/css/all.min.css')
-require('@/../node_modules/datatables.net-bs5/css/dataTables.bootstrap5.min.css')
+import '@/../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+import '@/../node_modules/datatables.net-bs5/css/dataTables.bootstrap5.min.css';
 
 extendMoment(moment);
 
