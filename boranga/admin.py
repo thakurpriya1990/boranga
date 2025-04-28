26c1c570531176082de25196e8fbf9a0bfb5a0aa
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-04-28 09:31:51 +0800

    Add first name and last name fields to SystemGroupPermission admin. This will run 2 more queries per row so is not great for performance. I think with the amount of records and how often the data will be accessed/modified it should still be ok.

--- boranga/admin.py
+++ boranga/admin.py
@@ -8,7 +8,6 @@ from django.contrib.gis import admin
 from django.forms import ValidationError
 from django.http import HttpResponse
 from django.urls import path
-from django.utils.functional import cached_property
 from ledger_api_client.admin import SystemGroupAdmin, SystemGroupPermissionInline
 from ledger_api_client.ledger_models import EmailUserRO as EmailUser
 from ledger_api_client.managed_models import SystemGroup, SystemGroupPermission
@@ -168,20 +167,11 @@ class CustomSystemGroupPermissionInline(SystemGroupPermissionInline):
     )
     readonly_fields = ("first_name", "last_name")
 
-    @cached_property
-    def emailuser_obj(self, obj):
-        """
-        Get the emailuser object from the system group permission.
-        """
-        if obj.emailuser_id:
-            return EmailUser.objects.get(id=obj.emailuser_id)
-        return None
-
     def first_name(self, obj):
-        return self.emailuser_obj.first_name
+        return EmailUser.objects.get(id=obj.emailuser_id).first_name
 
     def last_name(self, obj):
-        return self.emailuser_obj.last_name
+        return EmailUser.objects.get(id=obj.emailuser_id).last_name
 
 
 def export_system_group_permissions(modeladmin, request, queryset):
