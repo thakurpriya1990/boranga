e2db09e04599dea43390123eb0775c7c51f2846d
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-05-12 10:36:22 +0800

    Bug fix: Show missing info svg.

--- boranga/frontend/boranga/src/components/common/invite_external_referee.vue
+++ boranga/frontend/boranga/src/components/common/invite_external_referee.vue
@@ -98,26 +98,15 @@
                             </div>
                         </div>
 
-                        <alert class="alert alert-primary">
-                            <symbol
-                                id="info-fill"
-                                fill="currentColor"
-                                viewBox="0 0 16 16"
-                            >
-                                <path
-                                    d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"
-                                />
-                            </symbol>
+                        <alert
+                            class="d-flex alert alert-primary align-items-center"
+                        >
+                            <div class="flex-shrink-0 me-3">
+                                <h4>
+                                    <i class="bi bi-info-circle-fill"></i>
+                                </h4>
+                            </div>
                             <ul class="list-group">
-                                <svg
-                                    class="bi flex-shrink-0 text-primary mb-3"
-                                    width="24"
-                                    height="24"
-                                    role="img"
-                                    aria-label="Info:"
-                                >
-                                    <use xlink:href="#info-fill" />
-                                </svg>
                                 <li class="list-group-item">
                                     When you click 'Send Invite', the external
                                     referee will be sent an invite email.
