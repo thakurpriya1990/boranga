3916029eee2a02c59158c8514dfb1d5ec63a8760
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-06-04 15:10:18 +0800

    Remove invalid isNan check as it always returns true for a date string. Was put in by accident.

--- boranga/frontend/boranga/src/components/common/occurrence/occurrence_report_profile.vue
+++ boranga/frontend/boranga/src/components/common/occurrence/occurrence_report_profile.vue
@@ -592,21 +592,22 @@ export default {
         'occurrence_report_obj.observation_date': function () {
             let vm = this;
             if (vm.isFauna) {
+                console.log('Updating observation date for fauna');
                 if (
                     vm.occurrence_report_obj &&
                     vm.occurrence_report_obj.animal_observation &&
-                    vm.occurrence_report_obj.observation_date &&
-                    !isNaN(vm.occurrence_report_obj.observation_date)
+                    vm.occurrence_report_obj.observation_date
                 ) {
                     vm.occurrence_report_obj.animal_observation.obs_date =
                         vm.occurrence_report_obj.observation_date;
                 }
             } else if (vm.isCommunity) {
+                console.log('Updating observation date for community');
+
                 if (
                     vm.occurrence_report_obj &&
                     vm.occurrence_report_obj.habitat_condition &&
-                    vm.occurrence_report_obj.observation_date &&
-                    !isNaN(vm.occurrence_report_obj.observation_date)
+                    vm.occurrence_report_obj.observation_date
                 ) {
                     vm.occurrence_report_obj.habitat_condition.obs_date =
                         vm.occurrence_report_obj.observation_date;
@@ -615,9 +616,12 @@ export default {
                 if (
                     vm.occurrence_report_obj &&
                     vm.occurrence_report_obj.plant_count &&
-                    vm.occurrence_report_obj.observation_date &&
-                    !isNaN(vm.occurrence_report_obj.observation_date)
+                    vm.occurrence_report_obj.observation_date
                 ) {
+                    console.log(
+                        'Updating plant count observation date',
+                        vm.occurrence_report_obj.observation_date
+                    );
                     vm.occurrence_report_obj.plant_count.obs_date =
                         vm.occurrence_report_obj.observation_date;
                 }
